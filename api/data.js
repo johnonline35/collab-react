01bda46111e3de3d5ded834b4fdf15a09ef61a72
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-05 13:40:47 +1000

    enrich.tool testing api

---
+++ api/data.js
@@ -0,0 +1,64 @@
+const obj = {
+  status: "Success",
+  content: {
+    first_name: "John",
+    last_name: "Childs-Eddy",
+    summary: "Entrepreneur.",
+    location: "San Francisco, California, United States",
+    email: "johnchildseddy@gmail.com",
+    phone: null,
+    linkedin: "https://www.linkedin.com/in/johnchildseddy",
+    facebook: null,
+    job: [
+      {
+        company: "Collab Inc.",
+        position: "Founder & CEO",
+        location: "San Francisco Bay Area",
+        linkedin_url:
+          "https://www.linkedin.com/search/results/companies/?keywords=Collab Inc.",
+        date_from: "3-2023",
+        date_to: null,
+      },
+      {
+        company: "Skyflow",
+        position: "Head of EMEA & APAC",
+        location: null,
+        linkedin_url: "https://www.linkedin.com/company/19041178/",
+        date_from: "2021",
+        date_to: "2022",
+      },
+      {
+        company: "Stealth Startup",
+        position: "Co-Founder",
+        location: "Palo Alto, California, United States",
+        linkedin_url: "https://www.linkedin.com/company/18583501/",
+        date_from: "2020",
+        date_to: "2021",
+      },
+    ],
+    education: [
+      {
+        degree_name: "MBA",
+        field_of_study: null,
+        school_name: "University of Pennsylvania - The Wharton School",
+        date_from: "2012",
+        date_to: "2013",
+      },
+      {
+        degree_name: "MBA",
+        field_of_study: "Business Administration and Management, General",
+        school_name: "Australian Graduate School of Management",
+        date_from: "2010",
+        date_to: "2014",
+      },
+      {
+        degree_name: "Full Stack Software Development",
+        field_of_study:
+          "Javascript, React, Next.js, Chakra UI, Express, Node, Ruby, Rails",
+        school_name: "General Assembly",
+        date_from: "2022",
+        date_to: "2023",
+      },
+    ],
+  },
+};
