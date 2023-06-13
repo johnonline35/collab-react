ff60a1f1b21e713c49f33b964e0012b3b08fe671
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-13 14:48:33 +1000

    people data labs testing

--- api/brandFetch.js
+++ api/brandFetch.js
@@ -9,12 +9,8 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const brandfetch_api_key = process.env.REACT_APP_BRANDFETCH;
 
 module.exports = async (req, res) => {
-  console.log("Function started execution");
-
   // Extract newRow from the parsed body
   const newRow = req.body;
-
-  console.log("Received request body: ", newRow);
 
   // Extract domainName from newRow
   const domainName = newRow?.record?.domain;
@@ -22,68 +18,53 @@ module.exports = async (req, res) => {
   // extract collab_user_id from the request body
   const collabUserId = newRow?.record?.collab_user_id;
 
-  console.log(
-    "Attempting to upsert job queue with collabUserId: ",
-    collabUserId
-  );
-
-  // Upsert a new row into the job_queue table
-  const { data: upsertData, error: upsertError } = await supabase
-    .from("job_queue")
-    .upsert([{ collab_user_id: collabUserId, status: "job_started" }], {
-      onConflict: "job_id",
-    });
-
-  if (upsertError) {
-    console.error("Error upserting job:", upsertError);
-    res.status(500).send({ error: upsertError.message });
-    return;
-  }
-
-  console.log("Upsert job queue successful: ", upsertData);
-
-  // Log the domainName
-  console.log("Domain name: ", domainName);
-
-  // Check if the domainName parameter is an empty string
-  if (!domainName) {
-    console.log("No domain name was passed into the function");
-    res.status(400).json({ error: "Missing domainName parameter" });
-    return;
-  }
-
-  console.log("Checking if domain exists in the brandfetch_data table");
-
-  // Check if the domain name exists in the brandfetch_data table
-  const { data, error } = await supabase
-    .from("brandfetch_data")
-    .select("domain")
-    .eq("domain", domainName);
-
-  if (error) {
-    console.error(
-      "Error fetching data from brandfetch_data table: ",
-      error.message
-    );
-    res.status(500).json({ error: error.message });
-    return;
-  }
-
-  console.log("Fetched data from brandfetch_data table: ", data);
-
-  // If the domain exists in the table, return without doing anything
-  if (data.length > 0) {
-    console.log(`Domain name ${domainName} already exists in the table.`);
-    res.status(200).json({
-      message: `Domain name ${domainName} already exists in the table.`,
-    });
-    return;
-  }
-
-  console.log("Domain does not exist in the table, fetching brand data");
-
-  // If there is a domain and it doesn't exist in the brandfetch table, then retrieve it
   try {
+    // Upsert a new row into the job_queue table
+    const { data: upsertData, error: upsertJobError } = await supabase
+      .from("job_queue")
+      .upsert([{ collab_user_id: collabUserId, status: "job_started" }], {
+        onConflict: "job_id",
+      });
+
+    if (upsertJobError) {
+      console.error("Error upserting job:", upsertJobError);
+      res.status(500).send({ error: upsertJobError.message });
+      return;
+    }
+    // Log the domainName
+    console.log("Domain name: ", domainName);
+
+    // Check if the domainName parameter is an empty string
+    if (!domainName) {
+      console.log("No domain name was passed into the function");
+      res.status(400).json({ error: "Missing domainName parameter" });
+      return;
+    }
+
+    // Check if the domain name exists in the brandfetch_data table
+    const { data, error } = await supabase
+      .from("brandfetch_data")
+      .select("domain")
+      .eq("domain", domainName);
+
+    if (error) {
+      console.error(
+        "Error fetching data from brandfetch_data table: ",
+        error.message
+      );
+      res.status(500).json({ error: error.message });
+      return;
+    }
+
+    // If the domain exists in the table, return without doing anything
+    if (data.length > 0) {
+      console.log(`Domain name ${domainName} already exists in the table.`);
+      res.status(200).json({
+        message: `Domain name ${domainName} already exists in the table.`,
+      });
+      return;
+    }
+
     // Make a request to the Brandfetch API
     console.log("Making request to Brandfetch API");
     const response = await axios.get(
@@ -97,18 +78,16 @@ module.exports = async (req, res) => {
 
     console.log("Brandfetch Response:", response.data);
 
-    console.log("Attempting to upsert data into brandfetch_data table");
-
     // Upsert the response data into the brandfetch_data table
-    const { error: upsertError } = await supabase
+    const { error: upsertBrandError } = await supabase
       .from("brandfetch_data")
       .upsert(response.data, { returning: "minimal" }); // don't return the inserted row
 
-    if (upsertError) {
-      console.log("Error upserting data:", upsertError.message);
+    if (upsertBrandError) {
+      console.log("Error upserting data:", upsertBrandError.message);
       res
         .status(500)
-        .send(`Error upserting brand data: ${upsertError.message}`);
+        .send(`Error upserting brand data: ${upsertBrandError.message}`);
       return;
     }
 
@@ -120,6 +99,13 @@ module.exports = async (req, res) => {
     // Log the error message if the request failed
     console.log("Error fetching brand:", error.message);
     res.status(500).send(`Error fetching brand: ${error.message}`);
+  } finally {
+    // Update the job_queue table to 'job_complete' once the function execution is finished
+    await supabase
+      .from("job_queue")
+      .upsert([{ collab_user_id: collabUserId, status: "job_complete" }], {
+        onConflict: "job_id",
+      });
   }
 };
 
