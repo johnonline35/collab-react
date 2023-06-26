df4adfe0b7b82d253124910a38a61c05c8fd02da
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-06-26 16:45:37 +1000

    dashboard and collab page ui updates

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -277,7 +277,7 @@ export default function Dashboard() {
                 borderColor='blue.400'
                 bg='white'
                 loading={loadingCards}
-                minW='270px'
+                minW='md'
               >
                 <CardHeader
                   p='0'
