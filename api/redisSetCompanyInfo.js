c38d5498df8870e289721ff7355f6052650458aa
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-29 15:15:55 +1000

    adding redis to dashboard

--- api/redisSetCompanyInfo.js
+++ api/redisSetCompanyInfo.js
@@ -1,12 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const upstash = require("@upstash/redis");
+const { Redis } = require("@upstash/redis");
 
-// Establish a connection to the Upstash Redis instance
-const { redis, close } = upstash.connect({
-  rest: {
-    url: process.env.REACT_APP_UPSTASH_REDIS_REST_URL,
-    token: process.env.REACT_APP_UPSTASH_REDIS_REST_TOKEN,
-  },
+const redis = new Redis({
+  url: process.env.REACT_APP_UPSTASH_REDIS_REST_URL,
+  token: process.env.REACT_APP_UPSTASH_REDIS_REST_TOKEN,
 });
 
 module.exports = async (req, res) => {
@@ -19,19 +16,16 @@ module.exports = async (req, res) => {
     const response = await redis.set(userId, JSON.stringify(data));
 
     // Log the response status code and result of the operation
-    console.log(`Response status from Redis: ${response.status}`);
-    console.log(`Response data from Redis: ${JSON.stringify(response.data)}`);
+    console.log(`Response status from Redis: ${response}`);
+    console.log(`Response data from Redis: ${JSON.stringify(response)}`);
 
     // Close the connection to the Redis instance
-    close();
+    await redis.disconnect();
 
     res.status(200).send("Successfully stored company info in Redis");
   } catch (error) {
     // Log the error message if something goes wrong
     console.log(`Error occurred: ${error.message}`);
-
-    // Close the connection to the Redis instance
-    close();
 
     res.status(500).send("Something went wrong");
   }
