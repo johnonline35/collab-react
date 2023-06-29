e9afdbba78b210e422d50633d733a37d89ed8e35
John Childs-Eddy <johnchildseddy@gmail.com>
2023-06-29 15:48:24 +1000

    adding redis to dashboard

--- api/redisSetCompanyInfo.js
+++ api/redisSetCompanyInfo.js
@@ -8,29 +8,20 @@ const redis = new Redis({
 
 module.exports = async (req, res) => {
   try {
-    const { userId, data } = req.body;
+    const key = "myKey";
+    const value = "myValue";
 
-    // console.log(`Received request to store company info for user ${userId}`);
-    // console.log(`Data to be stored in Redis: ${JSON.stringify(data)}`);
+    console.log(`Received request to store key-value pair: ${key} - ${value}`);
 
-    // Set the company info in Redis using the JSON path and optional parameters
-    const response = await redis.json.set(
-      userId,
-      "$.path",
-      JSON.stringify(data),
-      { nx: true }
-    );
+    // Store the key-value pair in Redis
+    await redis.set(key, value);
 
-    console.log(`Response from Redis set operation: ${response}`);
-
-    // Log the response status code and result of the operation
-    console.log(`Response status from Redis: ${response}`);
-    console.log(`Response data from Redis: ${JSON.stringify(response)}`);
+    console.log(`Successfully stored key-value pair in Redis`);
 
     // Close the connection to the Redis instance
     await redis.disconnect();
 
-    res.status(200).send("Successfully stored company info in Redis");
+    res.status(200).send("Successfully stored key-value pair in Redis");
   } catch (error) {
     // Log the error message if something goes wrong
     console.error(`Error occurred: ${error.message}`);
@@ -39,3 +30,37 @@ module.exports = async (req, res) => {
     res.status(500).send("Something went wrong");
   }
 };
+
+// module.exports = async (req, res) => {
+//   try {
+//     const { userId, data } = req.body;
+
+//     // console.log(`Received request to store company info for user ${userId}`);
+//     // console.log(`Data to be stored in Redis: ${JSON.stringify(data)}`);
+
+//     // Set the company info in Redis using the JSON path and optional parameters
+//     const response = await redis.json.set(
+//       userId,
+//       "$.path",
+//       JSON.stringify(data),
+//       { nx: true }
+//     );
+
+//     console.log(`Response from Redis set operation: ${response}`);
+
+//     // Log the response status code and result of the operation
+//     console.log(`Response status from Redis: ${response}`);
+//     console.log(`Response data from Redis: ${JSON.stringify(response)}`);
+
+//     // Close the connection to the Redis instance
+//     await redis.disconnect();
+
+//     res.status(200).send("Successfully stored company info in Redis");
+//   } catch (error) {
+//     // Log the error message if something goes wrong
+//     console.error(`Error occurred: ${error.message}`);
+//     console.error(error);
+
+//     res.status(500).send("Something went wrong");
+//   }
+// };
