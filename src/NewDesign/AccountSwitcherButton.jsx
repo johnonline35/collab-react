0891b6d46c1c106660757c59c9fb06271f59b64c
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-06-30 15:13:35 +1000

    adding redis to dashboard

--- src/NewDesign/AccountSwitcherButton.jsx
+++ src/NewDesign/AccountSwitcherButton.jsx
@@ -1,39 +1,8 @@
 import { Box, Flex, HStack, Img, useMenuButton } from "@chakra-ui/react";
 import { HiSelector } from "react-icons/hi";
-import { supabase } from "../supabase/clientapp";
-import { useEffect, useState } from "react";
-import { useSession } from "../hooks/useSession";
 
 export const AccountSwitcherButton = (props) => {
   const buttonProps = useMenuButton(props);
-
-  const session = useSession();
-
-  const [avatarUrl, setAvatarUrl] = useState("");
-  const [userName, setUserName] = useState("");
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      if (!session) return;
-
-      const { user } = session;
-      console.log("User:", user);
-
-      const { data, error } = await supabase
-        .from("collab_users")
-        .select("collab_user_avatar_url, collab_user_name")
-        .eq("id", user.id) // Use the user's ID from the session
-        .single();
-
-      if (data && !error) {
-        setAvatarUrl(data.collab_user_avatar_url);
-        setUserName(data.collab_user_name);
-      }
-    };
-
-    fetchUserData();
-  }, [session]); // Refetch when the session changes
-
   return (
     <Flex
       as='button'
@@ -63,15 +32,15 @@ export const AccountSwitcherButton = (props) => {
           h='8'
           rounded='md'
           objectFit='cover'
-          src={avatarUrl}
-          alt={userName}
+          src='/img/linkedin_profile.jpeg'
+          alt='Chakra UI'
         />
         <Box textAlign='start'>
           <Box noOfLines={1} fontWeight='semibold'>
             Collab Inc.
           </Box>
           <Box fontSize='xs' color='blue.100'>
-            {userName}
+            John Childs-Eddy
           </Box>
         </Box>
       </HStack>
