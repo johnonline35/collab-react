470bfeb8f71748efb17bef81d523ae8782ce989a
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-07-02 13:59:15 +1000

    adding crud to ui

--- src/hooks/useLexicalMeetingInsert.js
+++ src/hooks/useLexicalMeetingInsert.js
@@ -1,6 +1,6 @@
 import { supabase } from "../supabase/clientapp";
 
-export const updateLexicalWithMeetingData = async (workspaceId) => {
+export const updateLexicalWithMeetingData = async (workspaceId, email) => {
   let jsonString =
     '{"root":{"children":[{"children":[{"detail":0,"format":1,"mode":"normal","style":"","text":"{workspaces.workspace_name} Meeting Notes","type":"text","version":1}],"direction":"ltr","format":"center","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Date: {meetings.start_datetime}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h2"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Attendees:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"collab_users.collab_user_name, collab_users.collab_user_job_title","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"attendees.attendee_name, attendees.attendee.job_title, attendees.attendee_linkedin, attendees.attendee_twitter","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Notes:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"children":[],"direction":"ltr","format":"","indent":0,"type":"listitem","version":1,"value":1}],"direction":null,"format":"","indent":0,"type":"list","version":1,"listType":"bullet","start":1,"tag":"ul"}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}';
 
@@ -27,7 +27,7 @@ export const updateLexicalWithMeetingData = async (workspaceId) => {
   const { data: collabUserData, error: collabUserError } = await supabase
     .from("collab_users")
     .select("collab_user_name, collab_user_job_title")
-    .eq("workspace_id", workspaceId)
+    .eq("collab_user_email", email)
     .single(); // Assuming there is only one collab_user per workspace
 
   if (collabUserError) throw collabUserError;
