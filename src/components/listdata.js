f0f05af753d697df1120ac61db952e61c47a1ee4
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-07-03 12:44:59 +1000

    adding crud to ui

---
+++ src/components/listdata.js
@@ -0,0 +1,30 @@
+export const posts = [
+  {
+    id: "1",
+    title: "Set up POC Meeting with Prosper",
+    publishedAt: "November 8, 2023",
+    text: "Liaise with Prosper from engineering in 2-days time",
+    todo_id: 12,
+    todo_title: "Create Q3 plan",
+    todo_text:
+      "Put together 10 slides on how I'm going to deepen engagement with this account",
+    todo_target_date: "November 15, 2023",
+  },
+  {
+    id: "2",
+    title: "Send 2-page API overview",
+    publishedAt: "October 23, 2023",
+    text: "Send to the entire data engineering team using eng-team@google.com",
+    todo_id: 13,
+    todo_title: "Write exec briefing document",
+    todo_text:
+      "Write 2-page briefing doc for regional SVP when she is town next week",
+    todo_target_date: "November 18, 2023",
+  },
+  {
+    id: "3",
+    title: "Wait for Mark to revert with MNDA",
+    publishedAt: "November 12, 2023",
+    text: "Mark needs  to run this through legal. Follow up on Friday if not heard back by then",
+  },
+];
