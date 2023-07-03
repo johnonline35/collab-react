ecfde5df916ec801ed98c306bfa32dd414aba6e5
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-07-03 13:21:06 +1000

    adding lexical nodes

--- src/hooks/useLexicalMeetingInsert.js
+++ src/hooks/useLexicalMeetingInsert.js
@@ -1,14 +1,9 @@
 import { supabase } from "../supabase/clientapp";
 import { formatTime } from "../hooks/useFormatTime";
+import { TextNode, ElementNode, RootNode } from "lexical";
 
 export const updateLexicalWithMeetingData = async (workspaceId) => {
-  let jsonString =
-    '{"root":{"children":[{"children":[{"detail":0,"format":1,"mode":"normal","style":"","text":"{workspaces.workspace_name} Meeting Notes","type":"text","version":1}],"direction":"ltr","format":"center","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"{meetings.start_datetime}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h2"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Attendees:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"attendees.attendee_name, attendees.attendee.job_title, attendees.attendee_linkedin, attendees.attendee_twitter","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Notes:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"children":[],"direction":"ltr","format":"","indent":0,"type":"listitem","version":1,"value":1}],"direction":null,"format":"","indent":0,"type":"list","version":1,"listType":"bullet","start":1,"tag":"ul"}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}';
-
-  // Convert jsonString into a JavaScript object
-  let jsonObj = JSON.parse(jsonString);
-
-  //   Query the workspace table for workspace name
+  // Query the workspace table for workspace name
   const { data: workspaceData, error: workspaceError } = await supabase
     .from("workspaces")
     .select("workspace_name")
@@ -17,17 +12,50 @@ export const updateLexicalWithMeetingData = async (workspaceId) => {
 
   if (workspaceError) throw workspaceError;
 
-  // Replace the workspace name placeholder
-  jsonObj.root.children[0].children[0].text =
-    jsonObj.root.children[0].children[0].text.replace(
-      "{workspaces.workspace_name}",
-      `${workspaceData.workspace_name}`
-    );
+  // Create Lexical nodes
+  const workspaceNameNode = new TextNode(`${workspaceData.workspace_name}`);
+  const headingNode = new ElementNode("heading");
+  headingNode.addChild(workspaceNameNode);
 
-  jsonString = JSON.stringify(jsonObj);
+  const rootNode = new RootNode();
+  rootNode.addChild(headingNode);
+
+  // Convert the rootNode to a JSON string
+  let jsonString = JSON.stringify(rootNode);
 
   return jsonString;
 };
+
+// import { supabase } from "../supabase/clientapp";
+// import { formatTime } from "../hooks/useFormatTime";
+
+// export const updateLexicalWithMeetingData = async (workspaceId) => {
+//   let jsonString =
+//     '{"root":{"children":[{"children":[{"detail":0,"format":1,"mode":"normal","style":"","text":"{workspaces.workspace_name} Meeting Notes","type":"text","version":1}],"direction":"ltr","format":"center","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"{meetings.start_datetime}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h2"},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Attendees:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"attendees.attendee_name, attendees.attendee.job_title, attendees.attendee_linkedin, attendees.attendee_twitter","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"quote","version":1},{"children":[],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Notes:","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1},{"children":[{"children":[],"direction":"ltr","format":"","indent":0,"type":"listitem","version":1,"value":1}],"direction":null,"format":"","indent":0,"type":"list","version":1,"listType":"bullet","start":1,"tag":"ul"}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}';
+
+//   // Convert jsonString into a JavaScript object
+//   let jsonObj = JSON.parse(jsonString);
+
+//   //   Query the workspace table for workspace name
+//   const { data: workspaceData, error: workspaceError } = await supabase
+//     .from("workspaces")
+//     .select("workspace_name")
+//     .eq("workspace_id", workspaceId)
+//     .single();
+
+//   if (workspaceError) throw workspaceError;
+
+//   // Replace the workspace name placeholder
+//   jsonObj.root.children[0].children[0].text =
+//     jsonObj.root.children[0].children[0].text.replace(
+//       "{workspaces.workspace_name}",
+//       `${workspaceData.workspace_name}`
+//     );
+
+//   jsonString = JSON.stringify(jsonObj);
+
+//   return jsonString;
+// };
 
 //   // Query the meetings table for next meeting time
 //   let currentDate = new Date();
