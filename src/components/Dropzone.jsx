9d9a2b1812b19e0dee021b389206f35cfffd1dff
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-07-05 12:52:55 +1000

    adding drag and drop

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -54,7 +54,7 @@ export const Dropzone = ({ userId, ...props }) => {
             const { data, error: updateError } = await supabase
               .from("collab_users")
               .update({ collab_user_avatar_url: publicURL })
-              .match({ user_id: userId });
+              .match({ id: userId });
 
             if (updateError) {
               throw updateError;
