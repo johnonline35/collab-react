cd9c928113734f9ac7129233b18934ea6d35e77b
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-01 12:43:45 +1000

    new blue avatar

--- src/hooks/useFormatTime.js
+++ src/hooks/useFormatTime.js
@@ -10,16 +10,53 @@ export function formatTime(timeString) {
     utcDate.getTime() - timezoneOffsetMinutes * 60 * 1000
   );
 
-  const options = { month: "long", day: "numeric" };
-  const dayMonth = new Intl.DateTimeFormat("en-US", options).format(localDate);
+  const currentYear = new Date().getFullYear();
+  const meetingYear = localDate.getFullYear();
 
-  const timeOptions = {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-    // timeZoneName: "short",
-  };
-  const time = new Intl.DateTimeFormat("en-US", timeOptions).format(localDate);
+  const options = { month: "long", day: "numeric", year: "numeric" };
+  const dayMonthYear = new Intl.DateTimeFormat("en-US", options).format(
+    localDate
+  );
 
-  return `${dayMonth} at ${time}`;
+  if (currentYear === meetingYear) {
+    const timeOptions = {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+      // timeZoneName: "short",
+    };
+    const time = new Intl.DateTimeFormat("en-US", timeOptions).format(
+      localDate
+    );
+
+    return `${dayMonthYear} at ${time}`;
+  } else {
+    return dayMonthYear;
+  }
 }
+
+// export function formatTime(timeString) {
+//   // Create a date object from the timeString
+//   const utcDate = new Date(timeString);
+
+//   // Get the timezone offset in minutes for the current locale
+//   const timezoneOffsetMinutes = new Date().getTimezoneOffset();
+
+//   // Convert the offset to milliseconds and subtract it from the UTC time to get the local time
+//   const localDate = new Date(
+//     utcDate.getTime() - timezoneOffsetMinutes * 60 * 1000
+//   );
+
+//   const options = { month: "long", day: "numeric" };
+//   const dayMonth = new Intl.DateTimeFormat("en-US", options).format(localDate);
+
+//   const timeOptions = {
+//     hour: "numeric",
+//     minute: "2-digit",
+//     hour12: true,
+//     // timeZoneName: "short",
+//   };
+//   const time = new Intl.DateTimeFormat("en-US", timeOptions).format(localDate);
+
+//   return `${dayMonth} at ${time}`;
+// }
