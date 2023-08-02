c81413ae733eed859da049c85d15f69aed5cd0ba
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-02 15:25:31 +1000

    new blue avatar

--- src/LexicalEditor/plugins/LocalStoragePlugin.js
+++ src/LexicalEditor/plugins/LocalStoragePlugin.js
@@ -20,21 +20,20 @@ export function LocalStoragePlugin(props) {
     // Load the state from local storage when the component first mounts
     const savedContent = localStorage.getItem(namespace);
     if (savedContent) {
-      const savedState = JSON.parse(savedContent);
-      editor.updateState(savedState);
+      const savedStateJSON = JSON.parse(savedContent);
+      const savedState = editor.parseEditorState(savedStateJSON);
+      editor.setEditorState(savedState); // Update the editor state
     }
 
-    return editor.registerUpdateListener(function ({
-      editorState,
-      dirtyElements,
-      dirtyLeaves,
-    }) {
-      // Don't update if nothing changed
-      if (dirtyElements.size === 0 && dirtyLeaves.size === 0) return;
+    return editor.registerUpdateListener(
+      ({ editorState, dirtyElements, dirtyLeaves }) => {
+        // Don't update if nothing changed
+        if (dirtyElements.size === 0 && dirtyLeaves.size === 0) return;
 
-      const serializedState = JSON.stringify(editorState);
-      debouncedSaveContent(serializedState);
-    });
+        const serializedState = JSON.stringify(editorState);
+        debouncedSaveContent(serializedState);
+      }
+    );
   }, [debouncedSaveContent, editor, namespace]);
 
   return null;
