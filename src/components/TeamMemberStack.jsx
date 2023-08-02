8d83627b3d066084ccefd2311acea40690c72be8
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-02 10:25:20 +1000

    new blue avatar

--- src/components/TeamMemberStack.jsx
+++ src/components/TeamMemberStack.jsx
@@ -18,6 +18,10 @@ import {
 } from "@chakra-ui/react";
 import { supabase } from "../supabase/clientapp";
 import { useParams } from "react-router-dom";
+import {
+  getTimeWithLocation,
+  capitalizeFirstLetterOfEachWord,
+} from "../util/timeAndCapitalize";
 
 const infoReducer = (state, action) => {
   switch (action.type) {
@@ -37,7 +41,7 @@ const TeamMemberStack = ({
 }) => {
   const [members, setMembers] = useState([]);
   const [info, dispatch] = useReducer(infoReducer, {});
-  const [isChecked, setIsChecked] = useState([]);
+
   const { workspace_id } = useParams();
 
   useEffect(() => {
@@ -77,72 +81,6 @@ const TeamMemberStack = ({
       console.error("Error updating attendee info:", error);
     }
   };
-
-  function getLocationByUTCOffset(offset) {
-    const timeZoneLocations = {
-      "-12": "Baker Island",
-      "-11": "Niue",
-      "-10": "Honolulu",
-      "-9": "Anchorage",
-      "-8": "San Francisco",
-      "-7": "Denver",
-      "-6": "Chicago",
-      "-5": "New York",
-      "-4": "Santiago",
-      "-3": "Buenos Aires",
-      "-2": "Fernando de Noronha",
-      "-1": "Azores",
-      0: "London",
-      1: "Paris",
-      2: "Cairo",
-      3: "Istanbul",
-      4: "Dubai",
-      5: "Karachi",
-      6: "Dhaka",
-      7: "Jakarta",
-      8: "Singapore",
-      9: "Seoul",
-      10: "Sydney",
-      11: "Noumea",
-      12: "Auckland",
-      13: "Apia",
-      14: "Kiritimati",
-    };
-
-    return timeZoneLocations[offset.toString()] || "Unknown";
-  }
-
-  function getTimeWithLocation(timeString) {
-    const [time, offsetString] = timeString.split("+");
-    const date = new Date(`1970-01-01T${time}Z`);
-
-    const hours = date.getUTCHours();
-    const minutes = date.getUTCMinutes();
-    const seconds = date.getUTCSeconds();
-
-    const ampm = hours >= 12 ? "PM" : "AM";
-    const hours12 = hours % 12 || 12;
-
-    const offset = parseInt(offsetString, 10);
-    const location = getLocationByUTCOffset(offset);
-
-    return {
-      time: `${hours12.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")} ${ampm}`,
-      location,
-    };
-  }
-
-  function capitalizeFirstLetterOfEachWord(str) {
-    if (str) {
-      return str.replace(/\b\w/g, function (char) {
-        return char.toUpperCase();
-      });
-    } else {
-      return "";
-    }
-  }
 
   return (
     <Center
@@ -328,96 +266,84 @@ const TeamMemberStack = ({
 
 export const MemoizedTeamMemberStack = React.memo(TeamMemberStack);
 
-//             return (
-//               <HStack key={member.attendee_id} alignItems='flex-start'>
-//                 {/* <Checkbox position='relative' left='-40px' /> */}
-//                 <Stack fontSize='sm' px='4' spacing='4'>
-//                   <Stack direction='row' justify='space-between' spacing='4'>
-//                     <HStack spacing='3'>
-//                       <Avatar
-//                         src={
-//                           member.attendee_avatar ||
-//                           "/images/custom/blue-avatar.jpeg"
-//                         }
-//                         boxSize='10'
-//                         // name={displayName}
-//                       >
-//                         {/* <AvatarBadge
-//                           boxSize='4'
-//                           bg={
-//                             member.workingHours === "yes"
-//                               ? "green.500"
-//                               : "red.500"
-//                           }
-//                         /> */}
-//                       </Avatar>
-//                       <Box>
-//                         <Flex direction='row' justify='space-between'>
-//                           <Editable
-//                             fontSize='sm'
-//                             onChange={handleNameChange}
-//                             onSubmit={handleNameSubmit}
-//                             defaultValue={capitalizeFirstLetterOfEachWord(
-//                               displayName
-//                             )}
-//                           >
-//                             <EditablePreview />
-//                             <EditableInput />
-//                           </Editable>
-//                           <Badge
-//                             size='sm'
-//                             colorScheme={
-//                               member.status === "lead" ? "green" : null
-//                             }
-//                           >
-//                             {member.status}
-//                           </Badge>
-//                         </Flex>
-//                         {/* <Text color='muted'>{member.attendee_job_title}</Text> */}
-//                         <Editable
-//                           color='muted'
-//                           fontSize='sm'
-//                           onChange={handleJobTitleChange}
-//                           onSubmit={handleJobTitleSubmit}
-//                           defaultValue={capitalizeFirstLetterOfEachWord(
-//                             displayTitle
-//                           )}
-//                         >
-//                           <EditablePreview />
-//                           <EditableInput />
-//                         </Editable>
-//                       </Box>
-//                     </HStack>
-//                     {/* <Text color='muted'>{member.lastSeen}</Text> */}
-//                   </Stack>
-//                   <Text
-//                     color='muted'
-//                     sx={{
-//                       "-webkit-box-orient": "vertical",
-//                       "-webkit-line-clamp": "2",
-//                       overflow: "hidden",
-//                       display: "-webkit-box",
-//                     }}
-//                   >
-//                     {/* Current Time: {time} {location} <br /> */}
-//                     {/* Email: {member.attendee_email} */}
-//                     <Editable
-//                       fontSize='sm'
-//                       // onChange={handleEmailChange}
-//                       // onSubmit={handleEmailSubmit}
-//                       isDisabled
-//                       defaultValue={member.attendee_email}
-//                     >
-//                       <EditablePreview />
-//                       <EditableInput />
-//                     </Editable>
-//                   </Text>
-//                 </Stack>
-//               </HStack>
-//             );
-//           })}
+// import React from "react";
+// import { useState, useEffect, useReducer } from "react";
+// import { Box, Center, Stack, StackDivider } from "@chakra-ui/react";
+// import { supabase } from "../supabase/clientapp";
+// import { useParams } from "react-router-dom";
+// import Member from "./MemberForMemberStack";
+
+// const infoReducer = (state, action) => {
+//   switch (action.type) {
+//     case "updateInfo":
+//       return {
+//         ...state,
+//         [action.id]: { ...state[action.id], ...action.update },
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// const TeamMemberStack = ({
+//   handleAttendeeCheckboxChange,
+//   attendeeIsChecked,
+// }) => {
+//   const [members, setMembers] = useState([]);
+//   const [info, dispatch] = useReducer(infoReducer, {});
+//   const { workspace_id } = useParams();
+
+//   useEffect(() => {
+//     if (!workspace_id) {
+//       return null; // Or replace with <Loading /> component
+//     }
+//     const fetchAttendees = async () => {
+//       console.log("fetchAttendees called");
+
+//       if (!workspace_id) {
+//         console.error("Invalid or missing parameters: workspace_id'");
+//         return;
+//       }
+
+//       const { data, error } = await supabase
+//         .from("attendees")
+//         .select("*")
+//         .eq("workspace_id", workspace_id);
+
+//       if (error) {
+//         console.error(error);
+//       } else {
+//         setMembers(data);
+//       }
+//     };
+
+//     fetchAttendees();
+//   }, [workspace_id]);
+
+//   return (
+//     <Center
+//       maxW='sm'
+//       mx='auto'
+//       py={{
+//         base: "4",
+//         md: "8",
+//       }}
+//     >
+//       <Box bg='bg-surface' py='4' w='100%'>
+//         <Stack divider={<StackDivider />} spacing='4'>
+//           {members.map((member) => (
+//             <Member
+//               key={member.attendee_id}
+//               member={member}
+//               dispatch={dispatch}
+//               handleAttendeeCheckboxChange={handleAttendeeCheckboxChange}
+//               attendeeIsChecked={attendeeIsChecked}
+//             />
+//           ))}
 //         </Stack>
 //       </Box>
 //     </Center>
 //   );
 // };
+
+// export const MemoizedTeamMemberStack = React.memo(TeamMemberStack);
