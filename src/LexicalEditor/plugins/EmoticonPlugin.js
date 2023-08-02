6c9e538d90e06ec5fb28b9da90cbc6a0dcdf736a
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-02 15:58:41 +1000

    new blue avatar

---
+++ src/LexicalEditor/plugins/EmoticonPlugin.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { $createEmoticonNode } from "./EmoticonNode";
+import { TextNode } from "lexical";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+
+function emoticonTransform(node) {
+  const textContent = node.getTextContent();
+  if (textContent === ":avo:") {
+    node.replace($createEmoticonNode("emoticon avo-emoticon", "avo"));
+  } else if (textContent === ":)") {
+    node.replace($createEmoticonNode("", "🙂"));
+  }
+}
+
+function useEmoticons(editor) {
+  useEffect(() => {
+    const removeTransform = editor.addTransform(TextNode, emoticonTransform);
+    return () => {
+      removeTransform();
+    };
+  }, [editor]);
+}
+
+export default function EmoticonPlugin() {
+  const [editor] = useLexicalComposerContext();
+  useEmoticons(editor);
+  return null;
+}
