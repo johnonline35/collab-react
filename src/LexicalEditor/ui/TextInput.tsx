3703150321399fe9d52bbb767839558759cf8853
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-08 12:32:20 +1000

    new lexical ui components

--- src/LexicalEditor/ui/TextInput.tsx
+++ src/LexicalEditor/ui/TextInput.tsx
@@ -1,18 +1,10 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+import "./Input.css";
 
-import './Input.css';
-
-import * as React from 'react';
-import {HTMLInputTypeAttribute} from 'react';
+import * as React from "react";
+import { HTMLInputTypeAttribute } from "react";
 
 type Props = Readonly<{
-  'data-test-id'?: string;
+  "data-test-id"?: string;
   label: string;
   onChange: (val: string) => void;
   placeholder?: string;
@@ -24,16 +16,16 @@ export default function TextInput({
   label,
   value,
   onChange,
-  placeholder = '',
-  'data-test-id': dataTestId,
-  type = 'text',
+  placeholder = "",
+  "data-test-id": dataTestId,
+  type = "text",
 }: Props): JSX.Element {
   return (
-    <div className="Input__wrapper">
-      <label className="Input__label">{label}</label>
+    <div className='Input__wrapper'>
+      <label className='Input__label'>{label}</label>
       <input
         type={type}
-        className="Input__input"
+        className='Input__input'
         placeholder={placeholder}
         value={value}
         onChange={(e) => {
