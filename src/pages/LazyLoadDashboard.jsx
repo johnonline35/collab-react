8551800b6a7f5f10794d2b90f29044f82815c20c
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-16 09:16:09 +1000

    reverted to pre v2 playground

--- src/pages/LazyLoadDashboard.jsx
+++ src/pages/LazyLoadDashboard.jsx
@@ -13,6 +13,8 @@ const LazyLoadDashboard = React.lazy(() => import("./Dashboard"));
 export function DashboardLoader() {
   const noOfCards = Array(20).fill(0);
 
+  // Some comment for saving
+
   return (
     <>
       {" "}
