3e787aab64dc38dc41ff35bb30d48a51b35d9d07
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-18 15:32:54 +1000

    changed all the icons in the playground and added the meeting details node

--- src/components/CollabPageSidebar.jsx
+++ src/components/CollabPageSidebar.jsx
@@ -1,18 +1,8 @@
-import {
-  AtSignIcon,
-  AttachmentIcon,
-  ChatIcon,
-  CalendarIcon,
-  EditIcon,
-  ArrowRightIcon,
-  SunIcon,
-  QuestionOutlineIcon,
-  HamburgerIcon,
-} from "@chakra-ui/icons";
+import { EditIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { List, ListIcon, ListItem } from "@chakra-ui/react";
 import { NavLink, useParams } from "react-router-dom";
-import { FiDollarSign, FiUsers, FiShare, FiHome } from "react-icons/fi";
-import { SlRocket, SlFolderAlt } from "react-icons/sl";
+import { FiHome } from "react-icons/fi";
+
 import { CgWebsite } from "react-icons/cg";
 
 export default function Sidebar() {
@@ -29,74 +19,14 @@ export default function Sidebar() {
           <ListIcon as={FiHome} color='white' /> Overview
         </NavLink>
       </ListItem>
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/team`}>
-          <ListIcon as={FiUsers} color='white' />
-          Team
-        </NavLink>
-      </ListItem> */}
+
       <ListItem>
         <NavLink to={`/collabs/${params.workspace_id}/notes`}>
           <ListIcon as={EditIcon} color='white' />
           Workspace AI
         </NavLink>
       </ListItem>
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/nextsteps`}>
-          <ListIcon as={ArrowRightIcon} color='white' />
-          Next Steps
-        </NavLink>
-      </ListItem> */}
-      {/* <ListItem>
-        <NavLink
-          to={`/collabs/${params.workspace_id}/${params.workspace_name}/journey`}
-        >
-          <ListIcon as={SlRocket} color='white' />
-          Journey
-        </NavLink>
-      </ListItem> */}
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/proposals`}>
-          <ListIcon as={SunIcon} color='white' />
-          Proposals
-        </NavLink>
-      </ListItem> */}
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/currentstate`}>
-          <ListIcon as={InfoOutlineIcon} color='white' />
-          Current State
-        </NavLink>
-      </ListItem> */}
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/legaldocuments`}>
-          <ListIcon as={AtSignIcon} color='white' />
-          Legal Documents
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/pricing`}>
-          <ListIcon as={FiDollarSign} color='white' />
-          Pricing
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/timeline`}>
-          <ListIcon as={CalendarIcon} color='white' />
-          Timeline(s)
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to={`/collabs/${params.attendee_company_id}/questions`}>
-          <ListIcon as={ChatIcon} color='white' />
-          Realtime Q&A
-        </NavLink>
-      </ListItem> */}
-      {/* <ListItem>
-        <NavLink to={`/collabs/${params.workspace_id}/files`}>
-          <ListIcon as={SlFolderAlt} color='white' />
-          Files
-        </NavLink>
-      </ListItem> */}
+
       <ListItem>
         <NavLink to={`/collabs/${params.workspace_id}/share`}>
           <ListIcon as={CgWebsite} color='white' />
---
+++ src/v2LexicalPlayground/nodes/GetMeetingDetailsNode.jsx
@@ -0,0 +1,160 @@
+import { $createParagraphNode, $createTextNode } from "lexical";
+import { $createHeadingNode, $createQuoteNode } from "@lexical/rich-text";
+import { $createLinkNode } from "@lexical/link";
+import { capitalizeFirstLetterOfEachWord } from "../utils/timeAndCapitalize";
+import { utcToZonedTime, format } from "date-fns-tz";
+
+function formatURL(url) {
+  if (!url.startsWith("http")) {
+    return "https://" + url;
+  }
+  return url;
+}
+
+function createLinkNodeWithText(url, text, title) {
+  url = formatURL(url);
+  const linkNode = $createLinkNode(url, {
+    target: "_blank",
+    title: title,
+  });
+  linkNode.append($createTextNode(text));
+  return linkNode;
+}
+
+export function $createMeetingDetailsNode(meetingDetails) {
+  const gmdNode = $createParagraphNode();
+
+  // Workspace Name Heading
+  gmdNode.append(
+    $createHeadingNode("h1")
+      .append(
+        $createTextNode(meetingDetails.workspaceName + " Notes").setStyle(
+          "font-weight: bold"
+        )
+      )
+      .setFormat("center")
+      .append($createParagraphNode())
+  );
+
+  // Next Meeting Date
+  const timeZone = meetingDetails.user_timezone;
+  const zonedDate = utcToZonedTime(
+    new Date(meetingDetails.nextMeetingDate),
+    timeZone
+  );
+  let formattedNextMeetingDate = format(zonedDate, "EEEE, MMMM d, h:mma", {
+    timeZone,
+  })
+    .replace("AM", "am")
+    .replace("PM", "pm");
+  if (
+    formattedNextMeetingDate.endsWith(":00am") ||
+    formattedNextMeetingDate.endsWith(":00pm")
+  ) {
+    formattedNextMeetingDate = formattedNextMeetingDate.replace(":00", "");
+  }
+  gmdNode.append(
+    $createHeadingNode("h2")
+      .append($createTextNode("Date: " + formattedNextMeetingDate))
+      .append($createParagraphNode())
+  );
+
+  const attendeesContainer = $createQuoteNode();
+  meetingDetails.attendees.forEach((attendee) => {
+    // Company Information
+    if (
+      attendee.attendee_domain ||
+      attendee.job_company_linkedin_url ||
+      attendee.job_company_twitter_url
+    ) {
+      const companyParagraph = $createParagraphNode();
+      companyParagraph.append(
+        $createTextNode(
+          meetingDetails.workspaceName + " Company Information | "
+        )
+      );
+
+      let companyLinks = [];
+
+      if (attendee.attendee_domain) {
+        companyLinks.push(
+          createLinkNodeWithText(
+            attendee.attendee_domain,
+            "Website",
+            "Company Website"
+          )
+        );
+      }
+      if (attendee.job_company_linkedin_url) {
+        companyLinks.push(
+          createLinkNodeWithText(
+            attendee.job_company_linkedin_url,
+            "LinkedIn",
+            "Company LinkedIn"
+          )
+        );
+      }
+      if (attendee.job_company_twitter_url) {
+        companyLinks.push(
+          createLinkNodeWithText(
+            attendee.job_company_twitter_url,
+            "Twitter",
+            "Company Twitter"
+          )
+        );
+      }
+
+      // Join links with ' | ' separator
+      for (let i = 0; i < companyLinks.length; i++) {
+        companyParagraph.append(companyLinks[i]);
+        if (i !== companyLinks.length - 1) {
+          // Not the last item
+          companyParagraph.append($createTextNode(" | "));
+        }
+      }
+
+      attendeesContainer.append(companyParagraph);
+    }
+
+    // Attendee Information
+    const attendeeParagraph = $createParagraphNode();
+    let attendeeText = "";
+    if (attendee.attendee_name) {
+      attendeeText = capitalizeFirstLetterOfEachWord(attendee.attendee_name);
+    }
+    if (attendee.attendee_job_title) {
+      const attendeeJobTitle = capitalizeFirstLetterOfEachWord(
+        attendee.attendee_job_title
+      );
+      attendeeText += attendeeText ? ", " + attendeeJobTitle : attendeeJobTitle;
+    }
+    if (attendeeText) {
+      attendeeParagraph.append($createTextNode(attendeeText));
+    }
+    if (attendee.attendee_linkedin) {
+      attendeeParagraph.append($createTextNode(" | "));
+      const linkedinLinkNode = createLinkNodeWithText(
+        attendee.attendee_linkedin,
+        "LinkedIn",
+        "LinkedIn Profile"
+      );
+      attendeeParagraph.append(linkedinLinkNode);
+    }
+    if (attendee.attendee_twitter) {
+      attendeeParagraph.append($createTextNode(" | "));
+      const twitterLinkNode = createLinkNodeWithText(
+        attendee.attendee_twitter,
+        "Twitter",
+        "Twitter Profile"
+      );
+      attendeeParagraph.append(twitterLinkNode);
+    }
+    attendeesContainer.append(attendeeParagraph);
+  });
+  gmdNode
+    .append(attendeesContainer)
+    .append($createParagraphNode())
+    .append($createParagraphNode());
+
+  return gmdNode;
+}
