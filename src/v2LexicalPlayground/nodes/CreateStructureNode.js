a227d105d57828238998bec34bed68500044adc7
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-22 15:28:09 +1000

    updated toolbar icons

--- src/v2LexicalPlayground/nodes/CreateStructureNode.js
+++ src/v2LexicalPlayground/nodes/CreateStructureNode.js
@@ -10,10 +10,18 @@ export function $createStructureNode() {
   const root = $getRoot();
 
   // Create an h3 heading node with the text "Notes:"
-  const heading = $createHeadingNode("h3");
-  heading.append($createTextNode("Notes:"));
-
+  const heading = $createHeadingNode("h3").append($createTextNode("Notes:"));
   root.append(heading);
 
+  // Create an empty paragraph node and append it
   const paragraph = $createParagraphNode();
+  root.append(paragraph);
+
+  // Create an empty bullet list and append it
+  const list = $createListNode("bullet");
+  root.append(list);
+
+  // Create another empty paragraph node and append it
+  const paragraph2 = $createParagraphNode();
+  root.append(paragraph2);
 }
