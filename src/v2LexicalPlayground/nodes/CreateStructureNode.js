f73e5f2eb176f00fb2b0aabcaa53694247288530
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-23 09:09:20 +1000

    working on lexical AI features

--- src/v2LexicalPlayground/nodes/CreateStructureNode.js
+++ src/v2LexicalPlayground/nodes/CreateStructureNode.js
@@ -1,62 +1,3 @@
-// import { $createParagraphNode, $getRoot, $createTextNode } from "lexical";
-// import { $createHeadingNode } from "@lexical/rich-text";
-// import { $createListItemNode, $createListNode } from "@lexical/list";
-
-// export function $createStructureNode() {
-//   const root = $getRoot();
-
-//   // Prepare the nodes to add
-//   const nodesToAdd = [];
-
-//   // "Notes:" Heading
-//   const notesHeading = $createHeadingNode("h3").append(
-//     $createTextNode("Notes:")
-//   );
-//   nodesToAdd.push(notesHeading);
-
-//   // Empty Paragraph
-//   nodesToAdd.push($createParagraphNode());
-
-//   // Bullet list with a single bullet
-//   const list1 = $createListNode("bullet");
-//   list1.append($createListItemNode().append($createTextNode("")));
-//   nodesToAdd.push(list1);
-
-//   // Empty Paragraph
-//   nodesToAdd.push($createParagraphNode());
-
-//   // "Follow up:" Heading
-//   const followUpHeading = $createHeadingNode("h3").append(
-//     $createTextNode("Follow up:")
-//   );
-//   nodesToAdd.push(followUpHeading);
-
-//   // Empty Paragraph
-//   nodesToAdd.push($createParagraphNode());
-
-//   // Another bullet list with a single bullet
-//   const list2 = $createListNode("bullet");
-//   list2.append($createListItemNode().append($createTextNode("")));
-//   nodesToAdd.push(list2);
-
-//   // Empty Paragraph
-//   nodesToAdd.push($createParagraphNode());
-
-//   // Check if there are any child nodes
-//   if (root.getLastChild() !== null) {
-//     let lastChild = root.getLastChild();
-//     for (let node of nodesToAdd) {
-//       lastChild.insertBefore(node);
-//       lastChild = node; // Move the pointer to the last added node
-//     }
-//   } else {
-//     // Append normally to the root
-//     for (let node of nodesToAdd) {
-//       root.append(node);
-//     }
-//   }
-// }
-
 import { $createParagraphNode, $getRoot, $createTextNode } from "lexical";
 import { $createHeadingNode } from "@lexical/rich-text";
 import { $createListItemNode, $createListNode } from "@lexical/list";
@@ -64,41 +5,97 @@ import { $createListItemNode, $createListNode } from "@lexical/list";
 export function $createStructureNode() {
   const root = $getRoot();
 
+  // Create nodes to add
+  const nodesToAdd = [];
+
   // "Notes:" Heading
   const notesHeading = $createHeadingNode("h3").append(
     $createTextNode("Notes:").setStyle("font-weight: bold")
   );
-  root.insertBefore(notesHeading);
+  nodesToAdd.push(notesHeading);
 
   // Empty Paragraph
-  const paragraph1 = $createParagraphNode();
-  root.insertBefore(paragraph1);
+  nodesToAdd.push($createParagraphNode());
 
   // Bullet list with a single bullet
   const list1 = $createListNode("bullet");
   list1.append($createListItemNode().append($createTextNode("")));
-  root.insertBefore(list1);
+  nodesToAdd.push(list1);
 
   // Empty Paragraph
-  const paragraph2 = $createParagraphNode();
-  root.insertBefore(paragraph2);
+  nodesToAdd.push($createParagraphNode());
 
   // "Follow up:" Heading
   const followUpHeading = $createHeadingNode("h3").append(
     $createTextNode("Action items:").setStyle("font-weight: bold")
   );
-  root.insertBefore(followUpHeading);
+  nodesToAdd.push(followUpHeading);
 
   // Empty Paragraph
-  const paragraph3 = $createParagraphNode();
-  root.insertBefore(paragraph3);
+  nodesToAdd.push($createParagraphNode());
 
   // Another bullet list with a single bullet
   const list2 = $createListNode("bullet");
   list2.append($createListItemNode().append($createTextNode("")));
-  root.insertBefore(list2);
+  nodesToAdd.push(list2);
 
   // Empty Paragraph
-  const paragraph4 = $createParagraphNode();
-  root.insertBefore(paragraph4);
+  nodesToAdd.push($createParagraphNode());
+
+  if (root.getFirstChild() !== null) {
+    let firstChild = root.getFirstChild();
+    for (let node of nodesToAdd) {
+      firstChild.insertBefore(node);
+    }
+  } else {
+    for (let node of nodesToAdd) {
+      root.append(node);
+    }
+  }
 }
+
+// import { $createParagraphNode, $getRoot, $createTextNode } from "lexical";
+// import { $createHeadingNode } from "@lexical/rich-text";
+// import { $createListItemNode, $createListNode } from "@lexical/list";
+
+// export function $createStructureNode() {
+//   const root = $getRoot();
+
+//   // "Notes:" Heading
+//   const notesHeading = $createHeadingNode("h3").append(
+//     $createTextNode("Notes:").setStyle("font-weight: bold")
+//   );
+//   root.append(notesHeading);
+
+//   // Empty Paragraph
+//   const paragraph1 = $createParagraphNode();
+//   root.append(paragraph1);
+
+//   // Bullet list with a single bullet
+//   const list1 = $createListNode("bullet");
+//   list1.append($createListItemNode().append($createTextNode("")));
+//   root.append(list1);
+
+//   // Empty Paragraph
+//   const paragraph2 = $createParagraphNode();
+//   root.append(paragraph2);
+
+//   // "Follow up:" Heading
+//   const followUpHeading = $createHeadingNode("h3").append(
+//     $createTextNode("Action items:").setStyle("font-weight: bold")
+//   );
+//   root.append(followUpHeading);
+
+//   // Empty Paragraph
+//   const paragraph3 = $createParagraphNode();
+//   root.append(paragraph3);
+
+//   // Another bullet list with a single bullet
+//   const list2 = $createListNode("bullet");
+//   list2.append($createListItemNode().append($createTextNode("")));
+//   root.append(list2);
+
+//   // Empty Paragraph
+//   const paragraph4 = $createParagraphNode();
+//   root.append(paragraph4);
+// }
