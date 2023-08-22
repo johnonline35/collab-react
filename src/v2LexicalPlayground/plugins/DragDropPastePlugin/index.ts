4888843a7179fc1196ec259dbed219d646ec18d5
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-22 11:04:10 +1000

    updated toolbar icons

--- src/v2LexicalPlayground/plugins/DragDropPastePlugin/index.ts
+++ src/v2LexicalPlayground/plugins/DragDropPastePlugin/index.ts
@@ -6,20 +6,25 @@
  *
  */
 
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {DRAG_DROP_PASTE} from '@lexical/rich-text';
-import {isMimeType, mediaFileReader} from '@lexical/utils';
-import {COMMAND_PRIORITY_LOW} from 'lexical';
-import {useEffect} from 'react';
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { DRAG_DROP_PASTE } from "@lexical/rich-text";
+import { isMimeType, mediaFileReader } from "@lexical/utils";
+import { COMMAND_PRIORITY_LOW } from "lexical";
+import { useEffect } from "react";
 
-import {INSERT_IMAGE_COMMAND} from '../ImagesPlugin';
+import { INSERT_IMAGE_COMMAND } from "../ImagesPlugin";
 
 const ACCEPTABLE_IMAGE_TYPES = [
-  'image/',
-  'image/heic',
-  'image/heif',
-  'image/gif',
-  'image/webp',
+  "image/",
+  "image/heic",
+  "image/heif",
+  "image/gif",
+  "image/webp",
+  "image/jpeg", // JPEG
+  "image/png", // PNG
+  "image/bmp", // BMP
+  "image/svg+xml", // SVG
+  "image/tiff", // TIFF
 ];
 
 export default function DragDropPaste(): null {
@@ -31,9 +36,9 @@ export default function DragDropPaste(): null {
         (async () => {
           const filesResult = await mediaFileReader(
             files,
-            [ACCEPTABLE_IMAGE_TYPES].flatMap((x) => x),
+            [ACCEPTABLE_IMAGE_TYPES].flatMap((x) => x)
           );
-          for (const {file, result} of filesResult) {
+          for (const { file, result } of filesResult) {
             if (isMimeType(file, ACCEPTABLE_IMAGE_TYPES)) {
               editor.dispatchCommand(INSERT_IMAGE_COMMAND, {
                 altText: file.name,
@@ -44,7 +49,7 @@ export default function DragDropPaste(): null {
         })();
         return true;
       },
-      COMMAND_PRIORITY_LOW,
+      COMMAND_PRIORITY_LOW
     );
   }, [editor]);
   return null;
