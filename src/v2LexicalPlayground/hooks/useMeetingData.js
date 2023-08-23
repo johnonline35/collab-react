4ae1b3478596cfc4e4cf3a4cdc1b493b47d11e32
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-24 09:24:13 +1000

    created insert build rapport command

--- src/v2LexicalPlayground/hooks/useMeetingData.js
+++ src/v2LexicalPlayground/hooks/useMeetingData.js
@@ -1,14 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { fetchLexicalMeetingData } from "../../util/database";
 
 export function useMeetingData(workspace_id) {
-  const [meetingData, setMeetingData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetchLexicalMeetingData(workspace_id).then((data) => {
-      setMeetingData(data);
+    fetchLexicalMeetingData(workspace_id).then((newData) => {
+      setData(newData);
     });
   }, [workspace_id]);
+
+  const meetingData = useMemo(() => data, [data]);
 
   return meetingData;
 }
