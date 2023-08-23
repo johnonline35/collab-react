ed7dd26056337982a3f9add82fae4faf9361bc2f
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-23 10:55:21 +1000

    working on lexical AI features

---
+++ src/v2LexicalPlayground/hooks/useMeetingData.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { fetchLexicalMeetingData } from "../../util/database";
+
+export function useMeetingData(workspace_id) {
+  const [meetingData, setMeetingData] = useState([]);
+
+  useEffect(() => {
+    fetchLexicalMeetingData(workspace_id).then((data) => {
+      setMeetingData(data);
+    });
+  }, [workspace_id]);
+
+  return meetingData;
+}
