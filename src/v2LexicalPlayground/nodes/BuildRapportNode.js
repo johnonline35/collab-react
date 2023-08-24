a83bd5ea2d5601da1c057ff8b964bd1af2e30ceb
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-08-25 09:20:19 +1000

    created insert build rapport command

--- src/v2LexicalPlayground/nodes/BuildRapportNode.js
+++ src/v2LexicalPlayground/nodes/BuildRapportNode.js
@@ -3,7 +3,7 @@ import { $createHeadingNode } from "@lexical/rich-text";
 import { $createListItemNode, $createListNode } from "@lexical/list";
 import { insertBeforeLastChild } from "../utils/insertBeforeLastChild";
 
-export function $buildRapportNode() {
+export function $buildRapportNode(responseContent) {
   // "Notes:" Heading
   const notesHeading = $createHeadingNode("h3").append(
     $createTextNode("Rapport Building Topics:").setStyle("font-weight: bold")
@@ -13,11 +13,54 @@ export function $buildRapportNode() {
   // Empty Paragraph
   insertBeforeLastChild($createParagraphNode());
 
-  // Bullet list with a single bullet
-  const list1 = $createListNode("bullet");
-  list1.append($createListItemNode().append($createTextNode("")));
-  insertBeforeLastChild(list1);
+  // Create a list for bullets
+  const bulletList = $createListNode("bullet");
+  const lines = responseContent.split("\n");
+
+  for (const line of lines) {
+    if (line.startsWith("- ")) {
+      // This is a bullet point
+      const bulletContent = line.substring(2); // Remove "- " from the start
+      const bullet = $createListItemNode().append(
+        $createTextNode(bulletContent)
+      );
+      bulletList.append(bullet);
+    } else if (line.trim() !== "") {
+      // This is a plain text
+      const text = $createParagraphNode().append($createTextNode(line));
+      insertBeforeLastChild(text);
+    }
+    // For new lines
+    insertBeforeLastChild($createParagraphNode());
+  }
+
+  // Add the bullet list to the document
+  insertBeforeLastChild(bulletList);
 
   // Empty Paragraph
   insertBeforeLastChild($createParagraphNode());
 }
+
+// import { $createParagraphNode, $getRoot, $createTextNode } from "lexical";
+// import { $createHeadingNode } from "@lexical/rich-text";
+// import { $createListItemNode, $createListNode } from "@lexical/list";
+// import { insertBeforeLastChild } from "../utils/insertBeforeLastChild";
+
+// export function $buildRapportNode() {
+//   // "Notes:" Heading
+//   const notesHeading = $createHeadingNode("h3").append(
+//     $createTextNode("Rapport Building Topics:").setStyle("font-weight: bold")
+//   );
+//   insertBeforeLastChild(notesHeading);
+
+//   // Empty Paragraph
+//   insertBeforeLastChild($createParagraphNode());
+
+//   // Bullet list with a single bullet
+//   const list1 = $createListNode("bullet");
+//   list1.append($createListItemNode().append($createTextNode("")));
+//   insertBeforeLastChild(list1);
+
+//   // Empty Paragraph
+//   insertBeforeLastChild($createParagraphNode());
+// }
