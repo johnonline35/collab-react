a202ed18a353d9e7842429b5b52bef9b1f61982d
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-29 16:43:03 +1000

    streaming data into lexical

--- src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
+++ src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
@@ -1,16 +1,35 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { createCommand, COMMAND_PRIORITY_EDITOR } from "lexical";
+import {
+  $createTextNode,
+  $createParagraphNode,
+  createCommand,
+  COMMAND_PRIORITY_EDITOR,
+} from "lexical";
 import { $buildRapportNode } from "../../nodes/BuildRapportNode";
 import { useEffect, useState } from "react";
 import socket from "../../../util/socket";
+import { $createHeadingNode } from "@lexical/rich-text";
+import { insertBeforeLastChild } from "../../utils/insertBeforeLastChild";
 
 export const INSERT_BUILD_RAPPORT_COMMAND = createCommand();
 
 export default function BuildRapportPlugin({ meetingData }) {
   const [editor] = useLexicalComposerContext();
   const [summary, setSummary] = useState("");
+  const [hasInsertedHeading, setHasInsertedHeading] = useState(false);
 
   useEffect(() => {
+    if (!hasInsertedHeading) {
+      editor.update(() => {
+        const notesHeading = $createHeadingNode("h3").append(
+          $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
+        );
+        insertBeforeLastChild(notesHeading);
+        insertBeforeLastChild($createParagraphNode());
+      });
+      setHasInsertedHeading(true); // Update the flag to ensure the heading isn't inserted again
+    }
+
     if (!meetingData || meetingData.length === 0) {
       console.log("No meeting data");
       return;
@@ -38,7 +57,6 @@ export default function BuildRapportPlugin({ meetingData }) {
 
         const data = await response.json();
         console.log("streaming data:", data);
-        // setSummary(data.content);
       } catch (error) {
         console.error("There was an error fetching the summary!", error);
       }
@@ -65,7 +83,7 @@ export default function BuildRapportPlugin({ meetingData }) {
       socket.off("responseChunk");
       socket.disconnect();
     };
-  }, [meetingData, editor]);
+  }, [meetingData, editor, hasInsertedHeading]);
 
   useEffect(() => {
     if (!summary) {
