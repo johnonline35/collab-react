336127f2e46ab130776173b43ab94a4e0482ff2c
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-30 08:57:30 +1000

    streaming data into lexical

--- src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
+++ src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
@@ -27,16 +27,16 @@ export default function BuildRapportPlugin({ meetingData }) {
     console.log("Use effect called, about to call backend endpoint next.");
 
     // Insert the heading here, right after the initial checks.
-    if (!hasInsertedHeading) {
-      editor.update(() => {
-        const notesHeading = $createHeadingNode("h3").append(
-          $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
-        );
-        insertBeforeLastChild(notesHeading);
-        insertBeforeLastChild($createParagraphNode());
-      });
-      setHasInsertedHeading(true); // Update the flag to ensure the heading isn't inserted again
-    }
+    // if (!hasInsertedHeading) {
+    //   editor.update(() => {
+    //     const notesHeading = $createHeadingNode("h3").append(
+    //       $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
+    //     );
+    //     insertBeforeLastChild(notesHeading);
+    //     insertBeforeLastChild($createParagraphNode());
+    //   });
+    //   setHasInsertedHeading(true); // Update the flag to ensure the heading isn't inserted again
+    // }
 
     async function fetchSummary() {
       try {
