c222fccfe20c64b17fdf0adfa18590cddd68ab07
John Childs-Eddy <johnchildseddy@gmail.com>
2023-08-30 10:52:48 +1000

    streaming data into lexical

--- src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
+++ src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
@@ -55,6 +55,7 @@ export default function BuildRapportPlugin({
     if (!hasInsertedHeadingRef.current) {
       insertHeading();
       hasInsertedHeadingRef.current = true; // Update the flag to ensure the heading isn't inserted again
+      console.log("hasInsertedHeadingRef.current = true");
     }
 
     async function fetchSummary() {
