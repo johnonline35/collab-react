39d453e616eb1634b793240142d960dfa9495325
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-08-30 15:24:07 +1000

    streaming data into lexical

--- src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
+++ src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
@@ -22,29 +22,21 @@ export default function BuildRapportPlugin({
   const session = useSession();
   const [editor] = useLexicalComposerContext();
   const [summary, setSummary] = useState("");
-  const [hasInsertedHeading, setHasInsertedHeading] = useState(false);
+  const hasInsertedHeadingRef = useRef(false);
   const hasEffectRun = useRef(false);
 
-  // const insertHeading = () => {
-  //   console.log("insertHeading called");
+  const insertHeading = () => {
+    console.log("insertHeading called");
 
-  //   editor.update(() => {
-  //     console.log("editor.update called");
-  //     const notesHeading = $createHeadingNode("h3").append(
-  //       $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
-  //     );
-  //     insertBeforeLastChild(notesHeading);
-  //     insertBeforeLastChild($createParagraphNode());
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   // Check if the trigger is active and the heading hasn't been inserted yet
-  //   if (triggerEffect && !hasInsertedHeading) {
-  //     insertHeading();
-  //     setHasInsertedHeading(true); // Mark the heading as inserted
-  //   }
-  // }, [triggerEffect]);
+    editor.update(() => {
+      console.log("editor.update called");
+      const notesHeading = $createHeadingNode("h3").append(
+        $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
+      );
+      insertBeforeLastChild(notesHeading);
+      insertBeforeLastChild($createParagraphNode());
+    });
+  };
 
   useEffect(() => {
     console.log("useEffect fired:", triggerEffect);
@@ -69,34 +61,38 @@ export default function BuildRapportPlugin({
     console.log("All checks passed and useEffect succesfully called.");
 
     // Insert the heading here, right after the initial checks.
-    // if (!hasInsertedHeadingRef.current) {
-    //   insertHeading();
-    //   hasInsertedHeadingRef.current = true; // Update the flag to ensure the heading isn't inserted again
-    // }
+    if (!hasInsertedHeadingRef.current) {
+      insertHeading();
+      hasInsertedHeadingRef.current = true; // Update the flag to ensure the heading isn't inserted again
+    }
 
     async function fetchSummary() {
-      try {
-        console.log("fetch called");
-        const response = await fetch(
-          "https://collab-express-production.up.railway.app/summarize-career-education",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(meetingData),
+      setTimeout(async () => {
+        try {
+          console.log("fetch called");
+          const response = await fetch(
+            "https://collab-express-production.up.railway.app/summarize-career-education",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(meetingData),
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error(
+              `Server responded with a ${response.status} status.`
+            );
           }
-        );
 
-        if (!response.ok) {
-          throw new Error(`Server responded with a ${response.status} status.`);
+          const data = await response.json();
+          console.log("streaming data:", data);
+        } catch (error) {
+          console.error("There was an error fetching the summary!", error);
         }
-
-        const data = await response.json();
-        console.log("streaming data:", data);
-      } catch (error) {
-        console.error("There was an error fetching the summary!", error);
-      }
+      }, 2000);
     }
 
     // Establish a connection and listen for events from the backend
@@ -117,7 +113,7 @@ export default function BuildRapportPlugin({
     fetchSummary(); // fetch initial data
 
     hasEffectRun.current = true;
-    // hasInsertedHeadingRef.current = false;
+    hasInsertedHeadingRef.current = false;
 
     // Clean up listeners and disconnect on component unmount or if meetingData changes
     return () => {
@@ -135,16 +131,7 @@ export default function BuildRapportPlugin({
     const unregister = editor.registerCommand(
       INSERT_BUILD_RAPPORT_COMMAND,
       () => {
-        editor.update(() => {
-          console.log("editor.update called");
-          const notesHeading = $createHeadingNode("h3").append(
-            $createTextNode("Pre-Meeting Research:").setStyle(
-              "font-weight: bold"
-            )
-          );
-          insertBeforeLastChild(notesHeading);
-          insertBeforeLastChild($createParagraphNode());
-        });
+        console.log("command set");
         return true;
       },
       COMMAND_PRIORITY_EDITOR
