d1f233397c1009ebca2401a85449f95ffb8a37b3
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-09-08 09:36:14 +1000

    updating lexical editor to insert response content at cursor position

--- src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
+++ src/v2LexicalPlayground/plugins/BuildRapportPlugin/index.js
@@ -45,6 +45,11 @@ export default function BuildRapportPlugin({ meetingData, triggerEffect }) {
   };
 
   useEffect(() => {
+    const WINDOW_KEY = `selection_${new Date().getTime()}`;
+    editor.update(() => {
+      window[WINDOW_KEY] = $getSelection().focus.key;
+    });
+
     console.log("useEffect fired:", triggerEffect);
     if (!hasEffectRun.current && !triggerEffect) {
       console.log("Effect early exit due to run check and trigger check.");
@@ -113,20 +118,15 @@ export default function BuildRapportPlugin({ meetingData, triggerEffect }) {
       (data) => {
         console.log("chunk data:", data);
         editor.update(() => {
-          const buildRapportNode = $buildRapportNode(data.content);
-          const selection = $getSelection();
-          selection?.insertNodes([buildRapportNode]);
-          // if (data.content.trim()) {
-          //   // only proceed if content is not empty
-          //   const buildRapportNode = $buildRapportNode(data.content);
-          //   $insertNodes([buildRapportNode]);
-          //   if ($isRootOrShadowRoot(buildRapportNode.getParentOrThrow())) {
-          //     $wrapNodeInElement(
-          //       buildRapportNode,
-          //       $createParagraphNode
-          //     ).selectEnd();
-          //   }
-          // }
+          $getRoot()
+            .getAllTextNodes()
+            .forEach((n) => {
+              if (n.getKey() === window[WINDOW_KEY]) {
+                n.getParent().append(
+                  $createTextNode(new Date().getTime() + " | ")
+                );
+              }
+            });
         });
 
         return true;
@@ -183,6 +183,168 @@ export default function BuildRapportPlugin({ meetingData, triggerEffect }) {
 
   return null;
 }
+
+// export default function BuildRapportPlugin({ meetingData, triggerEffect }) {
+//   const session = useSession();
+//   const [editor] = useLexicalComposerContext();
+//   const [summary, setSummary] = useState("");
+//   const hasInsertedHeadingRef = useRef(false);
+//   const hasEffectRun = useRef(false);
+
+//   const insertHeading = () => {
+//     console.log("insertHeading called");
+//     editor.update(() => {
+//       const root = $getRoot();
+//       const paragraph = $createParagraphNode();
+//       console.log("editor.update(() =>  called");
+//       const notesHeading = $createHeadingNode("h3").append(
+//         $createTextNode("Pre-Meeting Research:").setStyle("font-weight: bold")
+//       );
+//       root.append(notesHeading);
+//       root.append(paragraph);
+//     });
+//   };
+
+//   useEffect(() => {
+//     console.log("useEffect fired:", triggerEffect);
+//     if (!hasEffectRun.current && !triggerEffect) {
+//       console.log("Effect early exit due to run check and trigger check.");
+//       return;
+//     }
+//     if (!session) {
+//       console.error("User is not authenticated!");
+//       return;
+//     }
+//     console.log("session user object:", session);
+//     if (!meetingData || meetingData.length === 0) {
+//       console.log("Effect early exit due to no meeting data.");
+//       return;
+//     }
+//     console.log("All checks passed and useEffect succesfully called.");
+//     const unregisterInsertHeading = editor.registerCommand(
+//       INSERT_HEADING_COMMAND,
+//       () => {
+//         insertHeading();
+//         return true;
+//       },
+//       COMMAND_PRIORITY_EDITOR
+//     );
+
+//     if (!hasInsertedHeadingRef.current) {
+//       editor.dispatchCommand(INSERT_HEADING_COMMAND);
+//       hasInsertedHeadingRef.current = true;
+//     }
+
+//     async function fetchSummary() {
+//       try {
+//         console.log("fetch called");
+//         const response = await fetch(
+//           "https://collab-express-production.up.railway.app/summarize-career-education",
+//           {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(meetingData),
+//           }
+//         );
+//         if (!response.ok) {
+//           throw new Error(`Server responded with a ${response.status} status.`);
+//         }
+//         const data = await response.json();
+//         console.log("streaming data:", data);
+//       } catch (error) {
+//         console.error("There was an error fetching the summary!", error);
+//       }
+//     }
+
+//     // Registering the commands
+//     const unregisterConnect = editor.registerCommand(
+//       CONNECT_SOCKET_COMMAND,
+//       () => {
+//         console.log("Connected to backend");
+//         socket.emit("registerUser", session.user.id);
+//         return true;
+//       },
+//       COMMAND_PRIORITY_EDITOR
+//     );
+
+//     const unregisterAppendChunk = editor.registerCommand(
+//       APPEND_CHUNK_TO_EDITOR_COMMAND,
+//       (data) => {
+//         console.log("chunk data:", data);
+//         editor.update(() => {
+//           const buildRapportNode = $buildRapportNode(data.content);
+//           const selection = $getSelection();
+//           const selectionKey = selection.anchor.key;
+//           selection?.insertNodes([buildRapportNode]);
+//           // if (data.content.trim()) {
+//           //   // only proceed if content is not empty
+//           //   const buildRapportNode = $buildRapportNode(data.content);
+//           //   $insertNodes([buildRapportNode]);
+//           //   if ($isRootOrShadowRoot(buildRapportNode.getParentOrThrow())) {
+//           //     $wrapNodeInElement(
+//           //       buildRapportNode,
+//           //       $createParagraphNode
+//           //     ).selectEnd();
+//           //   }
+//           // }
+//         });
+
+//         return true;
+//       },
+//       COMMAND_PRIORITY_EDITOR
+//     );
+
+//     // Dispatching the commands
+//     socket.on("connect", () => {
+//       editor.dispatchCommand(CONNECT_SOCKET_COMMAND);
+//     });
+
+//     socket.on("responseChunk", (data) => {
+//       if (data.content.trim() === "") {
+//         console.log("Received empty content, ignoring.");
+//         return;
+//       }
+//       editor.dispatchCommand(APPEND_CHUNK_TO_EDITOR_COMMAND, data);
+//     });
+
+//     fetchSummary();
+
+//     hasEffectRun.current = true;
+//     hasInsertedHeadingRef.current = false;
+
+//     return () => {
+//       unregisterConnect();
+//       unregisterAppendChunk();
+//       unregisterInsertHeading();
+//       socket.off("responseChunk");
+//       socket.disconnect();
+//     };
+//   }, [meetingData, editor, triggerEffect, session]);
+
+//   useEffect(() => {
+//     if (!summary) {
+//       console.log("No summary data");
+//       return;
+//     }
+//     const unregister = editor.registerCommand(
+//       INSERT_BUILD_RAPPORT_COMMAND,
+//       () => {
+//         console.log("command set");
+//         return true;
+//       },
+//       COMMAND_PRIORITY_EDITOR
+//     );
+//     return () => {
+//       if (typeof unregister === "function") {
+//         unregister();
+//       }
+//     };
+//   }, [editor, summary]);
+
+//   return null;
+// }
 
 // import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 // import {
