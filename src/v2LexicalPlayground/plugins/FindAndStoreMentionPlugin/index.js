b0404a719c5577bf97b00786cd86c5fbd306dabe
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-09-12 09:43:41 +1000

    next step nodes

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -21,11 +21,13 @@ export default function FindAndStoreMentionPlugin() {
           if (element.__mention === "Next Step:") {
             const nextStepNode = element;
             const siblingNode = nextStepNode.getNextSibling();
-            const nextStepTextContent = siblingNode.getTextContent();
+            if (siblingNode.getTextContent()) {
+              const nextStepTextContent = siblingNode.getTextContent();
+              console.log("nextStepTextContent:", nextStepTextContent);
+            }
             console.log("Mention Node Type:", element.__mention);
             console.log("Mention Node UUID:", element.__uuid);
             console.log("siblingNode:", siblingNode);
-            console.log("nextStepTextContent:", nextStepTextContent);
           }
         });
       });
