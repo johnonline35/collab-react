91dff229046d884b20c01cf6f64b24ad90e698c6
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-09-12 09:34:50 +1000

    next step nodes

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -20,10 +20,11 @@ export default function FindAndStoreMentionPlugin() {
         allTextNodes.forEach((element) => {
           if (element.__mention === "Next Step:") {
             const nextStepNode = element;
+            const siblingNode = nextStepNode.getNextSibling();
 
             console.log("Mention Node Type:", element.__mention);
             console.log("Mention Node UUID:", element.__uuid);
-            console.log("nextStepNode:", nextStepNode);
+            console.log("siblingNode:", siblingNode);
           }
         });
       });
