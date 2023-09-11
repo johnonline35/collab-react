0bedd252c80576737e2f3bcab74efdabfbc92e9b
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-12 09:40:00 +1000

    next step nodes

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -21,10 +21,11 @@ export default function FindAndStoreMentionPlugin() {
           if (element.__mention === "Next Step:") {
             const nextStepNode = element;
             const siblingNode = nextStepNode.getNextSibling();
-
+            const nextStepTextContent = siblingNode.getTextContent();
             console.log("Mention Node Type:", element.__mention);
             console.log("Mention Node UUID:", element.__uuid);
             console.log("siblingNode:", siblingNode);
+            console.log("nextStepTextContent:", nextStepTextContent);
           }
         });
       });
