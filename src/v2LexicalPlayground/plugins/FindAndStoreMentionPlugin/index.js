0d62604198c5f3c2922c91c083961290b14d6b00
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-09-12 14:45:31 +1000

    next step nodes

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -9,13 +9,14 @@ export default function FindAndStoreMentionPlugin({ workspace_id, session }) {
 
   const [editor] = useLexicalComposerContext();
   const [uuidSet, setUuidSet] = useState(new Set());
-  const { userId } = session?.user?.id;
+  const userId = session?.user?.id;
 
   useEffect(() => {
     if (!userId || !workspace_id) {
       console.log("No userId or WorkspaceId found");
       return;
     }
+
     async function fetchData() {
       console.log("fetchData called");
       const fetchedUuids = await fetchUUIDs(workspace_id, userId);
