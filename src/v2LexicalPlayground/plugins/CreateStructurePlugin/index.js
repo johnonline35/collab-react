047c6e343f4cfeff6137532373599d00a59a78a6
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-15 09:33:23 +1000

    updating create structure node

--- src/v2LexicalPlayground/plugins/CreateStructurePlugin/index.js
+++ src/v2LexicalPlayground/plugins/CreateStructurePlugin/index.js
@@ -1,5 +1,26 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { createCommand, COMMAND_PRIORITY_EDITOR } from "lexical";
+import {
+  createCommand,
+  COMMAND_PRIORITY_EDITOR,
+  $createParagraphNode,
+  $createRangeSelection,
+  $getSelection,
+  $insertNodes,
+  $isNodeSelection,
+  $isRootOrShadowRoot,
+  $setSelection,
+  COMMAND_PRIORITY_EDITOR,
+  COMMAND_PRIORITY_HIGH,
+  COMMAND_PRIORITY_LOW,
+  createCommand,
+  DRAGOVER_COMMAND,
+  DRAGSTART_COMMAND,
+  DROP_COMMAND,
+  LexicalCommand,
+  LexicalEditor,
+  $createTextNode,
+} from "lexical";
+import { $insertNodeToNearestRoot } from "@lexical/utils";
 import { $createStructureNode } from "../../nodes/CreateStructureNode";
 import { useEffect } from "react";
 
@@ -14,7 +35,18 @@ export default function CreateStructurePlugin() {
       INSERT_STRUCTURE_COMMAND,
       () => {
         editor.update(() => {
-          $createStructureNode();
+          //
+          // $createStructureNode();
+          //
+          // const selection = $getSelection();
+          // if (!$isNodeSelection(selection)) {
+          //   return null;
+          // }
+          // const nodes = selection.getNodes();
+          // const node = nodes[0];
+          // console.log("Node for structure node:", node);
+          const textNode = $createTextNode("TEXT NODE");
+          $insertNodeToNearestRoot(textNode);
         });
         return true;
       },
