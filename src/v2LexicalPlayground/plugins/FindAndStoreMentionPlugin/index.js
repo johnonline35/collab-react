ac499fd50b1e17a731c2510f51aa5811be5e9c21
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-18 14:07:17 +1000

    debugging mention node uuid

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -3,179 +3,89 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { $getRoot } from "lexical";
 import { KEY_ENTER_COMMAND, COMMAND_PRIORITY_LOW } from "lexical";
 import { fetchUUIDs } from "../../../util/database";
-import { supabase } from "../../../supabase/clientapp";
 
 export default function FindAndStoreMentionPlugin({ workspace_id, session }) {
+  console.log("FindAndStoreMentionPlugin Called.");
+
   const [editor] = useLexicalComposerContext();
   const [uuidSet, setUuidSet] = useState(new Set());
+  const [nextStepUUID, setNextStepUUID] = useState(null);
+  const [nextStepContent, setNextStepContent] = useState(null);
   const userId = session?.user?.id;
 
+  // Fetch the existing mention UUID's and store them in a Set()
   useEffect(() => {
+    if (!userId || !workspace_id) {
+      console.log("No userId or WorkspaceId found");
+      return;
+    }
+
     async function fetchData() {
+      console.log("fetchData called");
       const fetchedUuids = await fetchUUIDs(workspace_id, userId);
+      console.log("fetchedUuids:", fetchedUuids);
       if (fetchedUuids) {
         setUuidSet(new Set(fetchedUuids));
       }
     }
 
-    if (userId && workspace_id) fetchData();
+    fetchData();
   }, [workspace_id, userId]);
 
-  async function upsertDataToSupabase(
-    extractedNextStepUUID,
-    extractedNextStepContent
-  ) {
-    try {
-      const { error } = await supabase.from("collab_users_next_steps").upsert({
-        collab_user_next_steps_id: extractedNextStepUUID,
-        workspace_id,
-        collab_user_id: userId,
-        nextstep_content: extractedNextStepContent,
-      });
-
-      if (error) {
-        console.error("Error upserting data:", error);
-      } else {
-        setUuidSet((prevSet) => new Set([...prevSet, extractedNextStepUUID]));
-      }
-    } catch (err) {
-      console.error("Supabase upsert error:", err);
-    }
-  }
-
-  function getMentionData(editorState) {
-    let extractedData = null;
-    editorState.read(() => {
-      const root = $getRoot();
-      const allTextNodes = root.getAllTextNodes();
-      for (const node of allTextNodes) {
-        if (node.__type === "mention" && node.__mention === "Next Steps:") {
-          const textContainerNode = node.getNextSibling();
-          if (
-            textContainerNode &&
-            textContainerNode.getTextContent() !== null
-          ) {
-            extractedData = {
-              extractedNextStepUUID: node.__uuid,
-              extractedNextStepContent: textContainerNode.getTextContent(),
-            };
-          }
-        }
-      }
-    });
-    return extractedData;
-  }
+  useEffect(() => {
+    console.log("Fetched UUID's:", uuidSet);
+  }, [uuidSet]);
 
   useEffect(() => {
-    if (!editor) return;
+    if (nextStepUUID && nextStepContent) {
+      console.log("Updated values:", nextStepUUID, nextStepContent);
+      // You can use them here.
+    }
+  }, [nextStepUUID, nextStepContent]);
 
+  useEffect(() => {
+    if (!editor) {
+      console.warn("Editor is not properly initialized.");
+      return;
+    }
+
+    // Listen to the editor state for new mentions:
     const unregister = editor.registerUpdateListener(({ editorState }) => {
-      getMentionData(editorState);
+      editorState.read(() => {
+        const root = $getRoot();
+        const allTextNodes = root.getAllTextNodes();
+        allTextNodes.forEach((node) => {
+          if (node.__type === "mention" && node.__mention === "Next Steps:") {
+            const targetMentionNode = node;
+            const textContainerNode = targetMentionNode.getNextSibling();
+            if (
+              textContainerNode &&
+              textContainerNode.getTextContent() !== null
+            ) {
+              setNextStepContent(textContainerNode.getTextContent());
+              setNextStepUUID(node.__uuid);
+            }
+          }
+        });
+      });
     });
 
+    // When the enter key is pressed, check the node tree for new UUID's and insert if needed:
     editor.registerCommand(
       KEY_ENTER_COMMAND,
-      ({ editorState }) => {
-        const mentionData = getMentionData(editorState);
-        if (mentionData && !uuidSet.has(mentionData.extractedNextStepUUID)) {
-          upsertDataToSupabase(
-            mentionData.extractedNextStepUUID,
-            mentionData.extractedNextStepContent
-          );
-        }
+      (event) => {
+        console.log("ENTER key pressed!");
         return false;
       },
       COMMAND_PRIORITY_LOW
     );
 
     return () => {
-      if (unregister) unregister();
+      if (unregister && typeof unregister === "function") {
+        unregister();
+      }
     };
-  }, [editor, uuidSet]);
+  }, [editor]);
 
   return null;
 }
-
-// import { useEffect, useState } from "react";
-// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-// import { $getRoot } from "lexical";
-// import { KEY_ENTER_COMMAND, COMMAND_PRIORITY_LOW } from "lexical";
-// import { fetchUUIDs } from "../../../util/database";
-
-// export default function FindAndStoreMentionPlugin({ workspace_id, session }) {
-//   console.log("FindAndStoreMentionPlugin Called.");
-
-//   const [editor] = useLexicalComposerContext();
-//   const [uuidSet, setUuidSet] = useState(new Set());
-//   const userId = session?.user?.id;
-
-//   // Fetch the existing mention UUID's and store them in a Set()
-//   useEffect(() => {
-//     if (!userId || !workspace_id) {
-//       console.log("No userId or WorkspaceId found");
-//       return;
-//     }
-
-//     async function fetchData() {
-//       console.log("fetchData called");
-//       const fetchedUuids = await fetchUUIDs(workspace_id, userId);
-//       console.log("fetchedUuids:", fetchedUuids);
-//       if (fetchedUuids) {
-//         setUuidSet(new Set(fetchedUuids));
-//       }
-//     }
-
-//     fetchData();
-//   }, [workspace_id, userId]);
-
-//   useEffect(() => {
-//     console.log("Fetched UUID's:", uuidSet);
-//   }, [uuidSet]);
-
-//   useEffect(() => {
-//     if (!editor) {
-//       console.warn("Editor is not properly initialized.");
-//       return;
-//     }
-
-//     // Listen to the editor state for new mentions:
-//     const unregister = editor.registerUpdateListener(({ editorState }) => {
-//       editorState.read(() => {
-//         const root = $getRoot();
-//         const allTextNodes = root.getAllTextNodes();
-//         allTextNodes.forEach((node) => {
-//           if (node.__type === "mention" && node.__mention === "Next Steps:") {
-//             const targetMentionNode = node;
-//             const textContainerNode = targetMentionNode.getNextSibling();
-//             if (
-//               textContainerNode &&
-//               textContainerNode.getTextContent() !== null
-//             ) {
-//               const extractedNextStepContent =
-//                 textContainerNode.getTextContent();
-//               const extractedNextStepUUID = node.__uuid;
-//             }
-//           }
-//         });
-//       });
-//     });
-
-//     // When the enter key is pressed, check the node tree for new UUID's and insert if needed:
-//     editor.registerCommand(
-//       KEY_ENTER_COMMAND,
-//       (event) => {
-//         console.log("ENTER key pressed!");
-//         return false;
-//       },
-//       COMMAND_PRIORITY_LOW
-//     );
-
-//     return () => {
-//       if (unregister && typeof unregister === "function") {
-//         unregister();
-//       }
-//     };
-//   }, [editor]);
-
-//   return null;
-// }
