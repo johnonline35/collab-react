0b058e8bf7edd63c0d3f4a9807b3de34e96b6aab
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-18 12:56:29 +1000

    debugging mention node uuid

--- src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
+++ src/v2LexicalPlayground/plugins/FindAndStoreMentionPlugin/index.js
@@ -39,12 +39,6 @@ export default function FindAndStoreMentionPlugin({ workspace_id, session }) {
       return;
     }
 
-    // HERE ARE THE INSTRUCTIONS TO CHAT GPT 4:
-    // get next step UUID list from supabase for the current userId and workspaceId - from here: Table "collab_users_next_steps"; select "collab_user_next_steps_id" eq "workspace_id" & collab_user_id
-    // use Set() to store them
-    // whenever there is an enter keydown event, check the Set() with the UUID of the
-    // THIS IS THE END  OF THE INSTRUCTIONS TO CHAT GPT 4
-
     // Listen to the editor state for new mentions:
     const unregister = editor.registerUpdateListener(({ editorState }) => {
       editorState.read(() => {
@@ -53,17 +47,14 @@ export default function FindAndStoreMentionPlugin({ workspace_id, session }) {
         allTextNodes.forEach((node) => {
           if (node.__type === "mention" && node.__mention === "Next Steps:") {
             const targetMentionNode = node;
-            console.log("Next Steps:", node);
             const textContainerNode = targetMentionNode.getNextSibling();
             if (
               textContainerNode &&
               textContainerNode.getTextContent() !== null
             ) {
-              const extractedTextContent = textContainerNode.getTextContent();
-              const extractedTextUUID = node.__uuid;
-              //   console.log("Mention Node:", node);
-              console.log("Mention Node UUID:", extractedTextUUID);
-              console.log("extractedTextContent:", extractedTextContent);
+              const extractedNextStepContent =
+                textContainerNode.getTextContent();
+              const extractedNextStepUUID = node.__uuid;
             }
           }
         });
