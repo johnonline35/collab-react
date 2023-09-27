6ca1bad8122559ee1d5626faa5adcc51ae70a3b6
John Childs-Eddy <johnchildseddy@gmail.com>
2023-09-27 14:58:34 +1000

    Added new customer decorator node

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -55,70 +55,66 @@ export const ToDoList = ({
     >
       <Box bg='bg-surface' py='4' w='100%'>
         <Stack divider={<StackDivider />} spacing='4'>
-          {toDoList
-            .filter((list) => list.ignore !== true)
-            .map((list) => (
-              <Flex
-                key={list.collab_user_todo_id}
-                fontSize='sm'
-                px='4'
-                spacing='0.5'
-                w='100%'
-                alignItems='center'
-              >
-                <Checkbox
-                  isChecked={isChecked.includes(list.collab_user_todo_id)}
-                  onChange={() =>
-                    handleCheckboxChange(list.collab_user_todo_id)
+          {toDoList.map((list) => (
+            <Flex
+              key={list.collab_user_todo_id}
+              fontSize='sm'
+              px='4'
+              spacing='0.5'
+              w='100%'
+              alignItems='center'
+            >
+              <Checkbox
+                isChecked={isChecked.includes(list.collab_user_todo_id)}
+                onChange={() => handleCheckboxChange(list.collab_user_todo_id)}
+                mr={5} // Add some margin to the right of the checkbox
+              />
+              <Stack w='100%'>
+                <Editable
+                  fontSize='sm'
+                  fontWeight='medium'
+                  color='emphasized'
+                  w='100%'
+                  onChange={(value) =>
+                    dispatch({
+                      type: "updateInfo",
+                      id: list.collab_user_todo_id,
+                      update: { todo_content: value },
+                    })
                   }
-                  mr={5} // Add some margin to the right of the checkbox
-                />
-                <Stack w='100%'>
-                  <Editable
-                    fontSize='sm'
-                    fontWeight='medium'
-                    color='emphasized'
-                    w='100%'
-                    onChange={(value) =>
-                      dispatch({
-                        type: "updateInfo",
-                        id: list.collab_user_todo_id,
-                        update: { todo_content: value },
-                      })
-                    }
-                    onSubmit={async (value) => {
-                      await updateToDoList(list.collab_user_todo_id, {
-                        todo_content: value,
-                      });
-                      setToDoList(
-                        toDoList.map((l) =>
-                          l.collab_user_todo_id === list.collab_user_todo_id
-                            ? { ...l, todo_content: value }
-                            : l
-                        )
-                      );
-                    }}
-                    defaultValue={list.todo_content}
-                  >
-                    <EditablePreview w='100%' />
-                    <EditableTextarea w='100%' resize='none' />
-                  </Editable>
-                  <Text
-                    color='subtle'
-                    sx={{
-                      "-webkit-box-orient": "vertical",
-                      "-webkit-line-clamp": "2",
-                      overflow: "hidden",
-                      display: "-webkit-box",
-                    }}
-                  >
-                    {list.collab_user_todo_id && (
-                      <>Todo created {formatDate(list.created_at)}</>
-                    )}
-                  </Text>
-                </Stack>
-              </Flex>
-            ))}
+                  onSubmit={async (value) => {
+                    await updateToDoList(list.collab_user_todo_id, {
+                      todo_content: value,
+                    });
+                    setToDoList(
+                      toDoList.map((l) =>
+                        l.collab_user_todo_id === list.collab_user_todo_id
+                          ? { ...l, todo_content: value }
+                          : l
+                      )
+                    );
+                  }}
+                  defaultValue={list.todo_content}
+                >
+                  <EditablePreview w='100%' />
+                  <EditableTextarea w='100%' resize='none' />
+                </Editable>
+                <Text
+                  color='subtle'
+                  sx={{
+                    "-webkit-box-orient": "vertical",
+                    "-webkit-line-clamp": "2",
+                    overflow: "hidden",
+                    display: "-webkit-box",
+                  }}
+                >
+                  {list.collab_user_todo_id && (
+                    <>Todo created {formatDate(list.created_at)}</>
+                  )}
+                </Text>
+              </Stack>
+            </Flex>
+          ))}
         </Stack>
       </Box>
     </Center>
