08e9e24762bd3a436e79856b6c703a47048aa878
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-09-27 11:37:00 +1000

    Added new customer decorator node

--- src/v2LexicalPlayground/nodes/CustomDecoratorNode.jsx
+++ src/v2LexicalPlayground/nodes/CustomDecoratorNode.jsx
@@ -54,32 +54,33 @@ function CustomDecoratorNodeComponent(props) {
   const [editor] = useLexicalComposerContext();
   const [spanText, setSpanText] = useState("");
 
-  useEffect(() => {
-    const delayedLoop = async (text) => {
-      let count = 0;
-
-      while (count < 5) {
-        count++;
-        const newSpanText = `${text}: ${count} `;
-        setSpanText(newSpanText);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      }
-    };
-
-    delayedLoop("hello");
-
-    // fetchData();
-  }, []);
-
   // useEffect(() => {
-  //   editor.update(() => {
-  //     const currentNode = $getNodeByKey(props.nodeKey);
-  //     const textNode = $createTextNode(spanText);
-  //     currentNode && currentNode.replace(textNode);
-  //   });
-  // }, [spanText]);
+  //   const delayedLoop = async (text) => {
+  //     let count = 0;
 
-  return <span spellCheck='false'>{spanText}</span>;
+  //     while (count < 5) {
+  //       count++;
+  //       const newSpanText = `${text}: ${count} `;
+  //       setSpanText(newSpanText);
+  //       await new Promise((resolve) => setTimeout(resolve, 1000));
+  //     }
+  //   };
+
+  //   delayedLoop("hello");
+
+  //   // fetchData();
+  // }, []);
+
+  useEffect(() => {
+    setSpanText("THIS");
+    editor.update(() => {
+      const currentNode = $getNodeByKey(props.nodeKey);
+      const textNode = $createTextNode(spanText);
+      currentNode && currentNode.replace(textNode);
+    });
+  }, [editor, props.nodeKey, spanText]);
+
+  return <span>{spanText}</span>;
 }
 
 /**
