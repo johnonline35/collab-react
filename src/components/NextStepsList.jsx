5f3f4b263f36f95628b56ce4b91a4c2f7a07cfdb
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-09-27 14:18:36 +1000

    Added new customer decorator node

--- src/components/NextStepsList.jsx
+++ src/components/NextStepsList.jsx
@@ -91,69 +91,71 @@ export const NextStepsList = ({
     >
       <Box bg='bg-surface' py='4' w='100%'>
         <Stack divider={<StackDivider />} spacing='4'>
-          {nextSteps.map((step) => (
-            <Flex
-              key={step.collab_user_next_steps_id}
-              fontSize='sm'
-              px='4'
-              spacing='0.5'
-              w='100%'
-              alignItems='center'
-            >
-              <Checkbox
-                isChecked={isChecked.includes(step.collab_user_next_steps_id)}
-                onChange={() =>
-                  handleCheckboxChange(step.collab_user_next_steps_id)
-                }
-                mr={5} // Add some margin to the right of the checkbox
-              />
-              <Stack w='100%'>
-                <Editable
-                  fontSize='sm'
-                  fontWeight='medium'
-                  color='emphasized'
-                  w='100%'
-                  onChange={(value) =>
-                    dispatch({
-                      type: "updateInfo",
-                      id: step.collab_user_next_steps_id,
-                      update: { nextstep_content: value },
-                    })
+          {nextSteps
+            .filter((step) => step.ignore !== true)
+            .map((step) => (
+              <Flex
+                key={step.collab_user_next_steps_id}
+                fontSize='sm'
+                px='4'
+                spacing='0.5'
+                w='100%'
+                alignItems='center'
+              >
+                <Checkbox
+                  isChecked={isChecked.includes(step.collab_user_next_steps_id)}
+                  onChange={() =>
+                    handleCheckboxChange(step.collab_user_next_steps_id)
                   }
-                  onSubmit={async (value) => {
-                    await updateNextStep(step.collab_user_next_steps_id, {
-                      nextstep_content: value,
-                    });
-                    setNextSteps(
-                      nextSteps.map((s) =>
-                        s.collab_user_next_steps_id ===
-                        step.collab_user_next_steps_id
-                          ? { ...s, nextstep_content: value }
-                          : s
-                      )
-                    );
-                  }}
-                  defaultValue={step.nextstep_content}
-                >
-                  <EditablePreview w='100%' />
-                  <EditableTextarea w='100%' resize='none' />
-                </Editable>
-                <Text
-                  color='subtle'
-                  sx={{
-                    "-webkit-box-orient": "vertical",
-                    "-webkit-line-clamp": "2",
-                    overflow: "hidden",
-                    display: "-webkit-box",
-                  }}
-                >
-                  {step.collab_user_next_steps_id && (
-                    <>Next step created {formatDate(step.created_at)}</>
-                  )}
-                </Text>
-              </Stack>
-            </Flex>
-          ))}
+                  mr={5} // Add some margin to the right of the checkbox
+                />
+                <Stack w='100%'>
+                  <Editable
+                    fontSize='sm'
+                    fontWeight='medium'
+                    color='emphasized'
+                    w='100%'
+                    onChange={(value) =>
+                      dispatch({
+                        type: "updateInfo",
+                        id: step.collab_user_next_steps_id,
+                        update: { nextstep_content: value },
+                      })
+                    }
+                    onSubmit={async (value) => {
+                      await updateNextStep(step.collab_user_next_steps_id, {
+                        nextstep_content: value,
+                      });
+                      setNextSteps(
+                        nextSteps.map((s) =>
+                          s.collab_user_next_steps_id ===
+                          step.collab_user_next_steps_id
+                            ? { ...s, nextstep_content: value }
+                            : s
+                        )
+                      );
+                    }}
+                    defaultValue={step.nextstep_content}
+                  >
+                    <EditablePreview w='100%' />
+                    <EditableTextarea w='100%' resize='none' />
+                  </Editable>
+                  <Text
+                    color='subtle'
+                    sx={{
+                      "-webkit-box-orient": "vertical",
+                      "-webkit-line-clamp": "2",
+                      overflow: "hidden",
+                      display: "-webkit-box",
+                    }}
+                  >
+                    {step.collab_user_next_steps_id && (
+                      <>Next step created {formatDate(step.created_at)}</>
+                    )}
+                  </Text>
+                </Stack>
+              </Flex>
+            ))}
         </Stack>
       </Box>
     </Center>
