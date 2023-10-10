68591477b8f563792be49ec4e4a5d0986e8ef829
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-10 16:13:52 +1100

    fixed individual sidebar

--- src/components/CollabPageSidebar.jsx
+++ src/components/CollabPageSidebar.jsx
@@ -1,31 +1,33 @@
+import { useState, useEffect } from "react";
 import { EditIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { Box, Text, List, ListIcon, ListItem } from "@chakra-ui/react";
 import { NavLink, useParams } from "react-router-dom";
 import { FiHome } from "react-icons/fi";
 import { CgWebsite } from "react-icons/cg";
+import { useSession } from "../hooks/useSession";
 
 export default function Sidebar() {
   const { workspace_id } = useParams();
-  // const session = useSession();
-  // const userId = session?.user.id;
-  // const [workspaceName, setWorkspaceName] = useState("");
+  const session = useSession();
+  const userId = session?.user.id;
+  const [workspaceName, setWorkspaceName] = useState("");
 
-  // useEffect(() => {
-  //   if (!session) return;
-  //   console.log("SESSION FFS:", session);
-  // }, [session]);
+  useEffect(() => {
+    if (!session) return;
+    console.log("SESSION FFS:", session);
+  }, [session]);
 
-  // useEffect(() => {
-  //   if (!userId || !workspace_id) return;
+  useEffect(() => {
+    if (!userId || !workspace_id) return;
 
-  //   async function fetchData() {
-  //     const name = "KEVIN";
-  //     // await fetchWorkspaceName(userId, workspace_id);
-  //     setWorkspaceName(name);
-  //   }
+    async function fetchData() {
+      const name = "KEVIN";
+      // await fetchWorkspaceName(userId, workspace_id);
+      setWorkspaceName(name);
+    }
 
-  //   fetchData();
-  // }, [userId, workspace_id]);
+    fetchData();
+  }, [userId, workspace_id]);
 
   return (
     <List color='white' fontSize='1.2em' spacing={4}>
@@ -37,7 +39,7 @@ export default function Sidebar() {
 
       <Box pl='45px'>
         <Text mb='15px' fontSize='l'>
-          Workspace
+          Workspace {workspaceName}
         </Text>
         <Box pl='17px'>
           <List spacing={4}>
@@ -66,42 +68,3 @@ export default function Sidebar() {
     </List>
   );
 }
-
-// import { EditIcon, HamburgerIcon } from "@chakra-ui/icons";
-// import { List, ListIcon, ListItem } from "@chakra-ui/react";
-// import { NavLink, useParams } from "react-router-dom";
-// import { FiHome } from "react-icons/fi";
-
-// import { CgWebsite } from "react-icons/cg";
-
-// export default function Sidebar() {
-//   const params = useParams();
-//   return (
-//     <List color='white' fontSize='1.2em' spacing={4}>
-//       <ListItem>
-//         <NavLink to='/dashboard'>
-//           <ListIcon as={HamburgerIcon} color='white' /> Dashboard
-//         </NavLink>
-//       </ListItem>
-//       <ListItem>
-//         <NavLink to={`/collabs/${params.workspace_id}`}>
-//           <ListIcon as={FiHome} color='white' /> Overview
-//         </NavLink>
-//       </ListItem>
-
-//       <ListItem>
-//         <NavLink to={`/collabs/${params.workspace_id}/notes`}>
-//           <ListIcon as={EditIcon} color='white' />
-//           Workspace AI
-//         </NavLink>
-//       </ListItem>
-
-//       <ListItem>
-//         <NavLink to={`/collabs/${params.workspace_id}/share`}>
-//           <ListIcon as={CgWebsite} color='white' />
-//           Collab Space
-//         </NavLink>
-//       </ListItem>
-//     </List>
-//   );
-// }
