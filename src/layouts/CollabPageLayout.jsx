77d4cacae7ab36f39c631e29f09a5e3a1b4397d7
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-10 14:23:52 +1100

    changes to api request body to account for email in meeting object

--- src/layouts/CollabPageLayout.jsx
+++ src/layouts/CollabPageLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CollabPageSidebar from "../components/CollabPageSidebar";
 
-export default function RootLayout() {
+export default function RootLayout(userId) {
   return (
     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
       <GridItem
@@ -13,10 +13,10 @@ export default function RootLayout() {
         minHeight={{ lg: "100vh" }}
         p={{ base: "20px", lg: "30px" }}
       >
-        <CollabPageSidebar />
+        <CollabPageSidebar userId={userId} />
       </GridItem>
       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
-        <Navbar />
+        <Navbar userId={userId} />
         <Outlet />
       </GridItem>
     </Grid>
