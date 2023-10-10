3e55146c840447ab4815217c2865314380f9b69b
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-10-10 15:43:53 +1100

    changes to api request body to account for email in meeting object

--- src/components/CollabPageSidebar.jsx
+++ src/components/CollabPageSidebar.jsx
@@ -3,8 +3,19 @@ import { Box, Text, List, ListIcon, ListItem } from "@chakra-ui/react";
 import { NavLink, useParams } from "react-router-dom";
 import { FiHome } from "react-icons/fi";
 import { CgWebsite } from "react-icons/cg";
+import { useSession } from "../hooks/useSession";
+import { fetchWorkspaceName } from "../util/database";
+import { useEffect } from "react";
 
-export default function Sidebar(userId, workspace_id, workspaceName) {
+export default async function Sidebar(workspace_id) {
+  const session = useSession();
+  const userId = session?.user.id;
+
+  useEffect(() => {
+    console.log("SESSION FFS:", session);
+  }, [session]);
+
+  const workspaceName = await fetchWorkspaceName(userId, workspace_id);
   return (
     <List color='white' fontSize='1.2em' spacing={4}>
       <ListItem>
