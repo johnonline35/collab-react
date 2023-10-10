f35d6091c757a9a12fb39f6c99b75f62e4951b19
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-10 14:33:53 +1100

    changes to api request body to account for email in meeting object

--- src/layouts/CollabPageLayout.jsx
+++ src/layouts/CollabPageLayout.jsx
@@ -1,9 +1,11 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CollabPageSidebar from "../components/CollabPageSidebar";
 
 export default function RootLayout(userId) {
+  const { workspace_id } = useParams();
+
   return (
     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
       <GridItem
@@ -13,7 +15,7 @@ export default function RootLayout(userId) {
         minHeight={{ lg: "100vh" }}
         p={{ base: "20px", lg: "30px" }}
       >
-        <CollabPageSidebar userId={userId} />
+        <CollabPageSidebar userId={userId} workspace_id={workspace_id} />
       </GridItem>
       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
         <Navbar userId={userId} />
