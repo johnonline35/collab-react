65bd0bca9e9bff96530aa40a1cf84d0e2fab7c60
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-10 12:37:18 +1100

    changes to api request body to account for email in meeting object

--- src/components/CollabPageSidebar.jsx
+++ src/components/CollabPageSidebar.jsx
@@ -1,8 +1,7 @@
 import { EditIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { List, ListIcon, ListItem } from "@chakra-ui/react";
+import { Box, Text, List, ListIcon, ListItem } from "@chakra-ui/react";
 import { NavLink, useParams } from "react-router-dom";
 import { FiHome } from "react-icons/fi";
-
 import { CgWebsite } from "react-icons/cg";
 
 export default function Sidebar() {
@@ -14,6 +13,14 @@ export default function Sidebar() {
           <ListIcon as={HamburgerIcon} color='white' /> Dashboard
         </NavLink>
       </ListItem>
+
+      {/* Heading for the Links */}
+      <Box my={4}>
+        <Text fontSize='xl' fontWeight='bold'>
+          Heading
+        </Text>
+      </Box>
+
       <ListItem>
         <NavLink to={`/collabs/${params.workspace_id}`}>
           <ListIcon as={FiHome} color='white' /> Overview
@@ -36,3 +43,42 @@ export default function Sidebar() {
     </List>
   );
 }
+
+// import { EditIcon, HamburgerIcon } from "@chakra-ui/icons";
+// import { List, ListIcon, ListItem } from "@chakra-ui/react";
+// import { NavLink, useParams } from "react-router-dom";
+// import { FiHome } from "react-icons/fi";
+
+// import { CgWebsite } from "react-icons/cg";
+
+// export default function Sidebar() {
+//   const params = useParams();
+//   return (
+//     <List color='white' fontSize='1.2em' spacing={4}>
+//       <ListItem>
+//         <NavLink to='/dashboard'>
+//           <ListIcon as={HamburgerIcon} color='white' /> Dashboard
+//         </NavLink>
+//       </ListItem>
+//       <ListItem>
+//         <NavLink to={`/collabs/${params.workspace_id}`}>
+//           <ListIcon as={FiHome} color='white' /> Overview
+//         </NavLink>
+//       </ListItem>
+
+//       <ListItem>
+//         <NavLink to={`/collabs/${params.workspace_id}/notes`}>
+//           <ListIcon as={EditIcon} color='white' />
+//           Workspace AI
+//         </NavLink>
+//       </ListItem>
+
+//       <ListItem>
+//         <NavLink to={`/collabs/${params.workspace_id}/share`}>
+//           <ListIcon as={CgWebsite} color='white' />
+//           Collab Space
+//         </NavLink>
+//       </ListItem>
+//     </List>
+//   );
+// }
