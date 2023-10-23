78782c7c60f76d1044cbeab0945c5e811a97e952
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-10-23 14:19:26 +1100

    added account switcher to top nav

--- src/layouts/CollabPageLayout.jsx
+++ src/layouts/CollabPageLayout.jsx
@@ -5,20 +5,31 @@ import CollabPageSidebar from "../components/CollabPageSidebar";
 
 export default function RootLayout() {
   return (
-    <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
-      <GridItem
-        as='aside'
-        colSpan={{ base: 6, lg: 2, xl: 1 }}
-        bg='blue.400'
-        minHeight={{ lg: "100vh" }}
-        p={{ base: "20px", lg: "30px" }}
-      >
-        <CollabPageSidebar />
-      </GridItem>
-      <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
+    <Grid templateColumns='repeat(6, 1fr)' bg='blue.400'>
+      <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='20px'>
         <Navbar />
         <Outlet />
       </GridItem>
     </Grid>
   );
 }
+
+// export default function RootLayout() {
+//   return (
+//     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
+//       <GridItem
+//         as='aside'
+//         colSpan={{ base: 6, lg: 2, xl: 1 }}
+//         bg='blue.400'
+//         minHeight={{ lg: "100vh" }}
+//         p={{ base: "20px", lg: "30px" }}
+//       >
+//         <CollabPageSidebar />
+//       </GridItem>
+//       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
+//         <Navbar />
+//         <Outlet />
+//       </GridItem>
+//     </Grid>
+//   );
+// }
