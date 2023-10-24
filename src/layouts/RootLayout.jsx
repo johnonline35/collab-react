0325a0c80c7a00f36521759b4071a812e36d2f10
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-10-24 14:12:17 +1100

    avatar changes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,25 +1,42 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import { NewApp } from "../NewDesign/NewApp";
 
 export default function RootLayout() {
   return (
     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
-      <GridItem
-        as='aside'
-        colSpan={{ base: 6, lg: 2, xl: 1 }}
-        bg='blue.400'
-        minHeight={{ lg: "100vh" }}
-        p={{ base: "20px", lg: "30px" }}
-      >
-        <NewApp />
-      </GridItem>
-      <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
+      <GridItem as='main' colSpan={6} p='40px'>
+        {" "}
+        {/* Adjusted to span the full grid width */}
         <Navbar />
         <Outlet />
       </GridItem>
     </Grid>
   );
 }
+
+// import { Grid, GridItem } from "@chakra-ui/react";
+// import { Outlet } from "react-router-dom";
+// import Navbar from "../components/Navbar";
+// import Sidebar from "../components/Sidebar";
+// import { NewApp } from "../NewDesign/NewApp";
+
+// export default function RootLayout() {
+//   return (
+//     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
+//       <GridItem
+//         as='aside'
+//         colSpan={{ base: 6, lg: 2, xl: 1 }}
+//         bg='blue.400'
+//         minHeight={{ lg: "100vh" }}
+//         p={{ base: "20px", lg: "30px" }}
+//       >
+//         <NewApp />
+//       </GridItem>
+//       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
+//         <Navbar />
+//         <Outlet />
+//       </GridItem>
+//     </Grid>
+//   );
+// }
