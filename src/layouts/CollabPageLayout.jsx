e434a3b1dcbde52e09afb0111917286611e5f764
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-24 15:11:15 +1100

    avatar changes

--- src/layouts/CollabPageLayout.jsx
+++ src/layouts/CollabPageLayout.jsx
@@ -1,35 +1,40 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import CollabPageSidebar from "../components/CollabPageSidebar";
+
+export default function RootLayout() {
+  return (
+    <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
+      <GridItem as='main' colSpan={6} p='20px' bg='blue.400' w='100%'>
+        {" "}
+        {/* Span the full width for the Navbar */}
+        <Navbar />
+      </GridItem>
+      <GridItem as='main' colSpan={6} p='40px'>
+        {" "}
+        {/* Span the full width for the Outlet */}
+        <Outlet />
+      </GridItem>
+    </Grid>
+  );
+}
 
 // export default function RootLayout() {
 //   return (
-//     <Grid templateColumns='repeat(6, 1fr)' bg='blue.400'>
-//       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='20px'>
+//     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
+//       <GridItem
+//         as='aside'
+//         colSpan={{ base: 6, lg: 2, xl: 1 }}
+//         bg='blue.400'
+//         minHeight={{ lg: "100vh" }}
+//         p={{ base: "20px", lg: "30px" }}
+//       >
+//         <CollabPageSidebar />
+//       </GridItem>
+//       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
 //         <Navbar />
 //         <Outlet />
 //       </GridItem>
 //     </Grid>
 //   );
 // }
-
-export default function RootLayout() {
-  return (
-    <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
-      <GridItem
-        as='aside'
-        colSpan={{ base: 6, lg: 2, xl: 1 }}
-        bg='blue.400'
-        minHeight={{ lg: "100vh" }}
-        p={{ base: "20px", lg: "30px" }}
-      >
-        <CollabPageSidebar />
-      </GridItem>
-      <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
-        <Navbar />
-        <Outlet />
-      </GridItem>
-    </Grid>
-  );
-}
