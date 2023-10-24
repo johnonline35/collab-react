229fc1bcd38df1b0ec6f40173e37837fd758f5b0
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-10-24 15:45:13 +1100

    Initial commit

--- src/NewDesign/AccountSwitcher.jsx
+++ src/NewDesign/AccountSwitcher.jsx
@@ -29,7 +29,7 @@ export const AccountSwitcher = () => {
 
   return (
     <Menu>
-      <AccountSwitcherButton session={session} />
+      <AccountSwitcherButton session={session} minWidth='80px' />
       <MenuList
         shadow='lg'
         py='4'
