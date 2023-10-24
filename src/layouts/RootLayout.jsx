366fbb153839aa381481de5b4044418d062fdb65
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-24 15:21:34 +1100

    avatar changes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
         {/* Span the full width for the Navbar */}
         <Navbar />
       </GridItem>
-      <GridItem as='main' colSpan={6} p='40px'>
+      <GridItem as='main' colSpan={6} pl='40px' pr='40px' pb='40px'>
         {" "}
         {/* Span the full width for the Outlet */}
         <Outlet />
