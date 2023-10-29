609a0fc3a2d7b6fae03595fb3b521b1d10dab7df
John Childs-Eddy <johnchildseddy@gmail.com>
2023-10-30 09:56:53 +1100

    Initial commit

--- src/pages/collabs/CollabPageHome.jsx
+++ src/pages/collabs/CollabPageHome.jsx
@@ -552,7 +552,7 @@ export default function CollabPageHome() {
                   }}
                   gap='6'
                 >
-                  <Card p='12px' variant='funky'>
+                  <Card p='12px' size='md'>
                     <List>
                       <ListItem fontSize='xl' fontWeight='bold'>
                         <Flex direction='row' justify='space-between'>
