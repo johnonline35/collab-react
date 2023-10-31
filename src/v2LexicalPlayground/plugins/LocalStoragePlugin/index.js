e38a3b2a6866a162d7fbc85dab92ecf0a96e83d5
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-11-01 09:50:48 +1100

    update notes

--- src/v2LexicalPlayground/plugins/LocalStoragePlugin/index.js
+++ src/v2LexicalPlayground/plugins/LocalStoragePlugin/index.js
@@ -5,13 +5,14 @@ import { debounce } from "lodash";
 
 export function LocalStoragePlugin() {
   const [editor] = useLexicalComposerContext();
-  const { workspace_id } = useParams();
+  const { workspace_id, collab_user_note_id } = useParams();
 
   const saveContent = useCallback(
     function (content) {
-      localStorage.setItem(workspace_id, content);
+      const localStorageKey = `content_${workspace_id}_${collab_user_note_id}`;
+      localStorage.setItem(localStorageKey, content);
     },
-    [workspace_id]
+    [workspace_id, collab_user_note_id]
   );
 
   const debouncedSaveContent = debounce(saveContent, 100);
@@ -21,35 +22,54 @@ export function LocalStoragePlugin() {
       ({ editorState, dirtyElements, dirtyLeaves }) => {
         // Don't update if nothing changed
         if (dirtyElements.size === 0 && dirtyLeaves.size === 0) return;
-        // console.log("editorState:", editorState);
-        // console.log(
-        //   "JSON.stringify(editorState):",
-        //   JSON.stringify(editorState)
-        // );
 
         const serializedState = JSON.stringify(editorState);
-        // editor.getEditorState().toJSON();
-        // JSON.stringify(editorState);
-        // console.log("Serialized Editor State:", serializedState);
-
         debouncedSaveContent(serializedState);
       }
     );
-  }, [debouncedSaveContent, editor, workspace_id]);
+  }, [debouncedSaveContent, editor, workspace_id, collab_user_note_id]);
 
   return null;
 }
 
-// Load the state from local storage when the component first mounts
-// const savedContent = localStorage.getItem(workspace_id);
-// if (savedContent) {
-//   console.log("Retrieved from local storage:", savedContent);
-//   const savedStateJSON = JSON.parse(savedContent);
-//   console.log(
-//     "Retrieved from local storage: savedStateJSON:",
-//     savedStateJSON
+// import { useCallback, useEffect } from "react";
+// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+// import { useParams } from "react-router-dom";
+// import { debounce } from "lodash";
+
+// export function LocalStoragePlugin() {
+//   const [editor] = useLexicalComposerContext();
+//   const { workspace_id } = useParams();
+
+//   const saveContent = useCallback(
+//     function (content) {
+//       localStorage.setItem(workspace_id, content);
+//     },
+//     [workspace_id]
 //   );
-//   const savedState = editor.parseEditorState(savedStateJSON);
-//   console.log("Parsed editor state:", savedState);
-//   editor.setEditorState(savedState); // Update the editor state
+
+//   const debouncedSaveContent = debounce(saveContent, 100);
+
+//   useEffect(() => {
+//     return editor.registerUpdateListener(
+//       ({ editorState, dirtyElements, dirtyLeaves }) => {
+//         // Don't update if nothing changed
+//         if (dirtyElements.size === 0 && dirtyLeaves.size === 0) return;
+//         // console.log("editorState:", editorState);
+//         // console.log(
+//         //   "JSON.stringify(editorState):",
+//         //   JSON.stringify(editorState)
+//         // );
+
+//         const serializedState = JSON.stringify(editorState);
+//         // editor.getEditorState().toJSON();
+//         // JSON.stringify(editorState);
+//         // console.log("Serialized Editor State:", serializedState);
+
+//         debouncedSaveContent(serializedState);
+//       }
+//     );
+//   }, [debouncedSaveContent, editor, workspace_id]);
+
+//   return null;
 // }
