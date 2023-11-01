4d40309ec2c4d61a8df24759005bd35965c840ae
John Childs-Eddy <johnchildseddy@johns-air.lan>
2023-11-01 14:51:23 +1100

    changing local storage

--- src/pages/collabs/CollabPageSettings.jsx
+++ src/pages/collabs/CollabPageSettings.jsx
@@ -49,7 +49,7 @@ export default function CollabPageSettings({
             gap='6'
           >
             {/* *pl='20px' pr='20px' pb='20px' */}
-            <Card p='12px'>
+            <Card p='5px'>
               <List>
                 {/* <ListItem fontSize='xl' fontWeight='bold'>
                   <ListIcon as={FiSettings} color='black' boxSize='24px' />
