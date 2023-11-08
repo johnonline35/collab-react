6d3fed2d521b91e7e0cf85c4794d38d89bae5fbe
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-11-08 12:30:18 +1100

    adding existing notes functions

--- src/v2LexicalPlayground/hooks/useMeetingData.js
+++ src/v2LexicalPlayground/hooks/useMeetingData.js
@@ -6,24 +6,52 @@ export function useMeetingData(workspace_id, collab_users_note_id) {
   const [meetingData, setMeetingData] = useState(null);
 
   useEffect(() => {
-    const note = supabase
-      .from("collab_users_notes")
-      .select("*")
-      .eq("collab_users_note_id", collab_users_note_id);
+    async function fetchData() {
+      try {
+        // Fetch the note data asynchronously
+        let { data: noteData, error } = await supabase
+          .from("collab_users_notes")
+          .select("*")
+          .eq("collab_users_note_id", collab_users_note_id)
+          .single(); // Assuming you are fetching a single item with a unique ID
 
-    const nextMeeting = note.data[0];
-    const nextMeetingId = nextMeeting.meeting_id;
+        if (error) {
+          throw error;
+        }
 
-    console.log({ nextMeetingId: nextMeetingId });
+        if (noteData) {
+          const nextMeetingId = noteData.meeting_id;
+          console.log({ nextMeetingId: nextMeetingId });
 
-    fetchLexicalMeetingData(
-      workspace_id,
-      collab_users_note_id,
-      nextMeetingId
-    ).then((data) => {
-      setMeetingData(data);
-    });
+          // Fetch additional data based on the meeting ID
+          const meetingInfo = await fetchLexicalMeetingData(
+            workspace_id,
+            collab_users_note_id,
+            nextMeetingId
+          );
+
+          setMeetingData(meetingInfo);
+        }
+      } catch (err) {
+        // Handle errors, for example by setting some error state or logging
+        console.error("Error fetching meeting data:", err);
+      }
+    }
+
+    fetchData();
   }, [workspace_id, collab_users_note_id]);
 
   return meetingData;
 }
+
+// export function useMeetingData(workspace_id, collab_users_note_id) {
+//   const [meetingData, setMeetingData] = useState(null);
+
+//   useEffect(() => {
+//     fetchLexicalMeetingData(workspace_id, collab_users_note_id).then((data) => {
+//       setMeetingData(data);
+//     });
+//   }, [workspace_id, collab_users_note_id]);
+
+//   return meetingData;
+// }
