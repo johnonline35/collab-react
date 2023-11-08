7cf570abf27d36f35c08d13218382c2b9ae64fba
John Childs-Eddy <johnchildseddy@gmail.com>
2023-11-08 12:13:50 +1100

    adding existing notes functions

--- src/v2LexicalPlayground/hooks/useMeetingData.js
+++ src/v2LexicalPlayground/hooks/useMeetingData.js
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 import { fetchLexicalMeetingData } from "../../utils/database";
+import { supabase } from "../../supabase/clientapp";
 
-export function useMeetingData(workspace_id) {
+export function useMeetingData(workspace_id, collab_users_note_id) {
   const [meetingData, setMeetingData] = useState(null);
 
   useEffect(() => {
-    fetchLexicalMeetingData(workspace_id).then((data) => {
+    const note = supabase
+      .from("collab_users_notes")
+      .select("*")
+      .eq("collab_users_note_id", collab_users_note_id);
+
+    const nextMeeting = note.data[0];
+    const nextMeetingId = nextMeeting.meeting_id;
+
+    console.log({ nextMeetingId: nextMeetingId });
+
+    fetchLexicalMeetingData(workspace_id, collab_users_note_id).then((data) => {
       setMeetingData(data);
     });
-  }, [workspace_id]);
+  }, [workspace_id, collab_users_note_id]);
 
   return meetingData;
 }
