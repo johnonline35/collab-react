ef65baf1189b975e275b8f422e3f00988b1ad487
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-11-15 08:52:03 +1100

    lifting function up to rootlayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,15 +2,49 @@ import React, { useState, useEffect } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useRecoilState } from "recoil";
+import {
+  avatarState,
+  companyNameState,
+  userNameState,
+} from "../atoms/avatarAtom";
+import { supabase } from "../supabase/clientapp";
 
-export default function RootLayout({
-  userEmail,
-  userId,
-  avatar,
-  userName,
-  companyName,
-  loading,
-}) {
+export default function RootLayout({ userEmail, userId }) {
+  const [avatar, setAvatar] = useRecoilState(avatarState);
+  const [userName, setUserName] = useRecoilState(userNameState);
+  const [companyName, setCompanyName] = useRecoilState(companyNameState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("collab_users")
+          .select("collab_user_avatar_url, collab_user_name, company_name")
+          .eq("id", userId)
+          .single();
+
+        if (error) {
+          console.error("Error fetching user data:", error);
+          return;
+        }
+
+        if (data) {
+          setAvatar(data.collab_user_avatar_url); // Update Recoil state
+          setUserName(data.collab_user_name);
+          setCompanyName(data.company_name);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, [userId, setAvatar, setCompanyName, setUserName]);
+
   return (
     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
       <GridItem as='main' colSpan={6} p='20px' bg='blue.400' w='100%'>
