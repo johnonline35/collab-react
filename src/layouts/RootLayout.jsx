7f56d27cf5ec2811aa973f1b0a1a3b4aee57cc1c
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-11-14 13:23:22 +1100

    adding userid

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -18,29 +18,3 @@ export default function RootLayout({ userEmail, userId }) {
     </Grid>
   );
 }
-
-// import { Grid, GridItem } from "@chakra-ui/react";
-// import { Outlet } from "react-router-dom";
-// import Navbar from "../components/Navbar";
-// import Sidebar from "../components/Sidebar";
-// import { NewApp } from "../NewDesign/NewApp";
-
-// export default function RootLayout() {
-//   return (
-//     <Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
-//       <GridItem
-//         as='aside'
-//         colSpan={{ base: 6, lg: 2, xl: 1 }}
-//         bg='blue.400'
-//         minHeight={{ lg: "100vh" }}
-//         p={{ base: "20px", lg: "30px" }}
-//       >
-//         <NewApp />
-//       </GridItem>
-//       <GridItem as='main' colSpan={{ base: 6, lg: 4, xl: 5 }} p='40px'>
-//         <Navbar />
-//         <Outlet />
-//       </GridItem>
-//     </Grid>
-//   );
-// }
