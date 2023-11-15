051fd381f05f80efdf2e32016ba4bb9dc5f14d7a
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-11-15 11:31:21 +1100

    improving meeting detials node in lexical

--- src/v2LexicalPlayground/nodes/GetMeetingDetailsNode.jsx
+++ src/v2LexicalPlayground/nodes/GetMeetingDetailsNode.jsx
@@ -26,17 +26,6 @@ export function $createMeetingDetailsNode(meetingDetails, publicEmailDomains) {
 
   let nodesToAdd = [];
 
-  // Workspace Name Heading
-  const workspaceNameNode = $createHeadingNode("h1")
-    .append(
-      $createTextNode(meetingDetails.workspaceName + " Notes").setStyle(
-        "font-weight: bold"
-      )
-    )
-    .setFormat("center");
-  nodesToAdd.push(workspaceNameNode);
-  nodesToAdd.push($createParagraphNode()); // An empty line after heading
-
   // Next Meeting Date
   const timeZone = meetingDetails.user_timezone;
   const zonedDate = utcToZonedTime(
@@ -174,3 +163,157 @@ export function $createMeetingDetailsNode(meetingDetails, publicEmailDomains) {
     }
   }
 }
+
+// export function $createMeetingDetailsNode(meetingDetails, publicEmailDomains) {
+//   const root = $getRoot();
+
+//   let nodesToAdd = [];
+
+//   // Workspace Name Heading
+//   const workspaceNameNode = $createHeadingNode("h1")
+//     .append(
+//       $createTextNode(meetingDetails.workspaceName).setStyle(
+//         "font-weight: bold"
+//       )
+//     )
+//     .setFormat("center");
+//   nodesToAdd.push(workspaceNameNode);
+//   nodesToAdd.push($createParagraphNode()); // An empty line after heading
+
+//   // Next Meeting Date
+//   const timeZone = meetingDetails.user_timezone;
+//   const zonedDate = utcToZonedTime(
+//     new Date(meetingDetails.nextMeetingDate),
+//     timeZone
+//   );
+//   let formattedNextMeetingDate = format(zonedDate, "EEEE, MMMM d, h:mma", {
+//     timeZone,
+//   })
+//     .replace("AM", "am")
+//     .replace("PM", "pm");
+//   if (
+//     formattedNextMeetingDate.endsWith(":00am") ||
+//     formattedNextMeetingDate.endsWith(":00pm")
+//   ) {
+//     formattedNextMeetingDate = formattedNextMeetingDate.replace(":00", "");
+//   }
+
+//   const meetingDateNode = $createHeadingNode("h2").append(
+//     $createTextNode(formattedNextMeetingDate)
+//   );
+//   nodesToAdd.push(meetingDateNode);
+//   nodesToAdd.push($createParagraphNode());
+
+//   const attendeesContainer = $createQuoteNode();
+
+//   // This flag will track if we've processed any valid attendees
+//   let hasValidAttendees = false;
+
+//   meetingDetails.attendees.forEach((attendee) => {
+//     if (
+//       !publicEmailDomains.includes(attendee.attendee_domain) &&
+//       (attendee.attendee_domain ||
+//         attendee.job_company_linkedin_url ||
+//         attendee.job_company_twitter_url)
+//     ) {
+//       hasValidAttendees = true; // We've found a valid attendee, so we'll set our flag to true
+
+//       const companyParagraph = $createParagraphNode();
+//       companyParagraph.append(
+//         $createTextNode(
+//           meetingDetails.workspaceName + " Company Information | "
+//         )
+//       );
+
+//       let companyLinks = [];
+//       if (attendee.attendee_domain) {
+//         companyLinks.push(
+//           createLinkNodeWithText(
+//             attendee.attendee_domain,
+//             "Website",
+//             "Company Website"
+//           )
+//         );
+//       }
+//       if (attendee.job_company_linkedin_url) {
+//         companyLinks.push(
+//           createLinkNodeWithText(
+//             attendee.job_company_linkedin_url,
+//             "LinkedIn",
+//             "Company LinkedIn"
+//           )
+//         );
+//       }
+//       if (attendee.job_company_twitter_url) {
+//         companyLinks.push(
+//           createLinkNodeWithText(
+//             attendee.job_company_twitter_url,
+//             "Twitter",
+//             "Company Twitter"
+//           )
+//         );
+//       }
+
+//       for (let i = 0; i < companyLinks.length; i++) {
+//         companyParagraph.append(companyLinks[i]);
+//         if (i !== companyLinks.length - 1) {
+//           companyParagraph.append($createTextNode(" | "));
+//         }
+//       }
+
+//       attendeesContainer.append(companyParagraph);
+//     }
+
+//     const attendeeParagraph = $createParagraphNode();
+//     let attendeeText = "";
+//     if (attendee.attendee_name) {
+//       attendeeText = capitalizeFirstLetterOfEachWord(attendee.attendee_name);
+//     }
+//     if (attendee.attendee_job_title) {
+//       const attendeeJobTitle = capitalizeFirstLetterOfEachWord(
+//         attendee.attendee_job_title
+//       );
+//       attendeeText += attendeeText ? ", " + attendeeJobTitle : attendeeJobTitle;
+//     }
+//     if (attendeeText) {
+//       attendeeParagraph.append($createTextNode(attendeeText));
+//     }
+//     if (attendee.attendee_linkedin) {
+//       attendeeParagraph.append($createTextNode(" | "));
+//       const linkedinLinkNode = createLinkNodeWithText(
+//         attendee.attendee_linkedin,
+//         "LinkedIn",
+//         "LinkedIn Profile"
+//       );
+//       attendeeParagraph.append(linkedinLinkNode);
+//     }
+//     if (attendee.attendee_twitter) {
+//       attendeeParagraph.append($createTextNode(" | "));
+//       const twitterLinkNode = createLinkNodeWithText(
+//         attendee.attendee_twitter,
+//         "Twitter",
+//         "Twitter Profile"
+//       );
+//       attendeeParagraph.append(twitterLinkNode);
+//     }
+//     attendeesContainer.append(attendeeParagraph);
+//   });
+
+//   // Here we check if we've processed any valid attendees. If we have, we append the attendeesContainer to nodesToAdd.
+//   if (hasValidAttendees) {
+//     nodesToAdd.push(attendeesContainer);
+//   }
+
+//   nodesToAdd.push($createParagraphNode());
+
+//   if (root.getFirstChild() !== null) {
+//     let firstChild = root.getFirstChild();
+//     for (let node of nodesToAdd) {
+//       firstChild.insertBefore(node);
+//     }
+//   } else {
+//     for (let node of nodesToAdd) {
+//       root.append(node);
+//     }
+//   }
+// }
