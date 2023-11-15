c896e13a6c28e1d76e6a1302c4c1e6f2f0eb29b8
John Childs-Eddy <johnchildseddy@gmail.com>
2023-11-15 13:41:49 +1100

    improving meeting detials node in lexical

--- src/v2LexicalPlayground/plugins/MeetingDetailsPlugin/index.jsx
+++ src/v2LexicalPlayground/plugins/MeetingDetailsPlugin/index.jsx
@@ -3,8 +3,6 @@ import { createCommand, COMMAND_PRIORITY_EDITOR } from "lexical";
 import { $createMeetingDetailsNode } from "../../nodes/GetMeetingDetailsNode";
 import { useEffect } from "react";
 
-export const INSERT_MEETING_DETAILS_COMMAND = createCommand();
-
 export default function MeetingDetailsPlugin({
   meetingData,
   publicEmailDomains,
@@ -17,26 +15,50 @@ export default function MeetingDetailsPlugin({
       return;
     }
 
-    const unregister = editor.registerCommand(
-      INSERT_MEETING_DETAILS_COMMAND,
-      () => {
-        editor.update(() => {
-          meetingData.forEach((m) => {
-            $createMeetingDetailsNode(m, publicEmailDomains);
-          });
-        });
-        return true;
-      },
-      COMMAND_PRIORITY_EDITOR
-    );
-
-    // If the registerCommand method returns a function to unregister the command, you can call it in the cleanup
-    return () => {
-      if (typeof unregister === "function") {
-        unregister();
-      }
-    };
+    editor.update(() => {
+      meetingData.forEach((m) => {
+        $createMeetingDetailsNode(m, publicEmailDomains);
+      });
+    });
   }, [editor, meetingData]);
 
   return null;
 }
+
+// export const INSERT_MEETING_DETAILS_COMMAND = createCommand();
+
+// export default function MeetingDetailsPlugin({
+//   meetingData,
+//   publicEmailDomains,
+// }) {
+//   const [editor] = useLexicalComposerContext();
+
+//   useEffect(() => {
+//     if (!meetingData || meetingData.length === 0) {
+//       console.log("No meeting data");
+//       return;
+//     }
+
+//     const unregister = editor.registerCommand(
+//       INSERT_MEETING_DETAILS_COMMAND,
+//       () => {
+//         editor.update(() => {
+//           meetingData.forEach((m) => {
+//             $createMeetingDetailsNode(m, publicEmailDomains);
+//           });
+//         });
+//         return true;
+//       },
+//       COMMAND_PRIORITY_EDITOR
+//     );
+
+//     // If the registerCommand method returns a function to unregister the command, you can call it in the cleanup
+//     return () => {
+//       if (typeof unregister === "function") {
+//         unregister();
+//       }
+//     };
+//   }, [editor, meetingData]);
+
+//   return null;
+// }
