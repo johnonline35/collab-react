8a247b7d21f613574debd03de1d930c9714cfcc7
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-11-16 12:57:55 +1100

    improving meeting detials node in lexical

--- src/pages/collabs/CollabPageHome.jsx
+++ src/pages/collabs/CollabPageHome.jsx
@@ -623,7 +623,7 @@ export default function CollabPageHome({ userId }) {
       {" "}
       <Flex gap={1}>
         {/* <Box position='relative'> */}
-        <Avatar bg='white' boxSize='7' src={workspaceLogo} />
+        <Avatar bg='white' boxSize='6' src={workspaceLogo} />
         {/* </Box> */}
         <Text fontSize='xl' as='b'>
           {customerName}
